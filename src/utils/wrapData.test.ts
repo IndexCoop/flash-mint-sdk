3d7f4dd0005430df8611c1349ed0412eb190325b
JD <jann.driessen@gmx.de>
2023-03-15 12:41:34 +0100

    feat: add wrap data

---
+++ src/utils/wrapData.test.ts
@@ -0,0 +1,39 @@
+import { getWrapData, IndexTokenMix } from './wrapData'
+
+const compoundWrapAdapterIntegrationName: string = 'CompoundWrapV2Adapter'
+const ZERO_BYTES = '0x0000000000000000000000000000000000000000'
+
+describe('getWrapData()', () => {
+  test('UNWRAPPED_ONLY', async () => {
+    const wrapData = getWrapData(IndexTokenMix.UNWRAPPED_ONLY)
+    expect(wrapData.length).toBe(3)
+    expect(wrapData[0].integrationName).toBe('')
+    expect(wrapData[1].integrationName).toBe('')
+    expect(wrapData[2].integrationName).toBe('')
+    expect(wrapData[0].wrapData).toBe(ZERO_BYTES)
+    expect(wrapData[1].wrapData).toBe(ZERO_BYTES)
+    expect(wrapData[2].wrapData).toBe(ZERO_BYTES)
+  })
+
+  test('WRAPPED_ONLY', async () => {
+    const wrapData = getWrapData(IndexTokenMix.WRAPPED_ONLY)
+    expect(wrapData.length).toBe(3)
+    expect(wrapData[0].integrationName).toBe(compoundWrapAdapterIntegrationName)
+    expect(wrapData[1].integrationName).toBe(compoundWrapAdapterIntegrationName)
+    expect(wrapData[2].integrationName).toBe(compoundWrapAdapterIntegrationName)
+    expect(wrapData[0].wrapData).toBe(ZERO_BYTES)
+    expect(wrapData[1].wrapData).toBe(ZERO_BYTES)
+    expect(wrapData[2].wrapData).toBe(ZERO_BYTES)
+  })
+
+  test('WRAPPED_UNWRAPPED_MIXED', async () => {
+    const wrapData = getWrapData(IndexTokenMix.WRAPPED_UNWRAPPED_MIXED)
+    expect(wrapData.length).toBe(3)
+    expect(wrapData[0].integrationName).toBe(compoundWrapAdapterIntegrationName)
+    expect(wrapData[1].integrationName).toBe('')
+    expect(wrapData[2].integrationName).toBe(compoundWrapAdapterIntegrationName)
+    expect(wrapData[0].wrapData).toBe(ZERO_BYTES)
+    expect(wrapData[1].wrapData).toBe(ZERO_BYTES)
+    expect(wrapData[2].wrapData).toBe(ZERO_BYTES)
+  })
+})
---
+++ src/utils/wrapData.ts
@@ -0,0 +1,63 @@
+export enum IndexTokenMix {
+  UNWRAPPED_ONLY = 'UNWRAPPED_ONLY',
+  WRAPPED_ONLY = 'WRAPPED_ONLY',
+  WRAPPED_UNWRAPPED_MIXED = 'WRAPPED_UNWRAPPED_MIXED',
+}
+
+export interface ComponentWrapData {
+  integrationName: string // wrap adapter integration name as listed in the IntegrationRegistry for the wrapModule
+  wrapData: string // optional wrapData passed to the wrapAdapter
+}
+
+const compoundWrapAdapterIntegrationName: string = 'CompoundWrapV2Adapter'
+const ZERO_BYTES = '0x0000000000000000000000000000000000000000'
+
+export function getWrapData(tokenMix: IndexTokenMix): ComponentWrapData[] {
+  switch (tokenMix) {
+    case IndexTokenMix.UNWRAPPED_ONLY:
+      return [
+        {
+          integrationName: '',
+          wrapData: ZERO_BYTES,
+        },
+        {
+          integrationName: '',
+          wrapData: ZERO_BYTES,
+        },
+        {
+          integrationName: '',
+          wrapData: ZERO_BYTES,
+        },
+      ]
+    case IndexTokenMix.WRAPPED_ONLY:
+      return [
+        {
+          integrationName: compoundWrapAdapterIntegrationName,
+          wrapData: ZERO_BYTES,
+        },
+        {
+          integrationName: compoundWrapAdapterIntegrationName,
+          wrapData: ZERO_BYTES,
+        },
+        {
+          integrationName: compoundWrapAdapterIntegrationName,
+          wrapData: ZERO_BYTES,
+        },
+      ]
+    case IndexTokenMix.WRAPPED_UNWRAPPED_MIXED:
+      return [
+        {
+          integrationName: compoundWrapAdapterIntegrationName,
+          wrapData: ZERO_BYTES,
+        },
+        {
+          integrationName: '',
+          wrapData: ZERO_BYTES,
+        },
+        {
+          integrationName: compoundWrapAdapterIntegrationName,
+          wrapData: ZERO_BYTES,
+        },
+      ]
+  }
+}
