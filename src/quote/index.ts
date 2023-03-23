f789b4abea7a89dca1422164f6223b3fb9330e13
JD <jann.driessen@gmx.de>
2023-03-23 09:21:35 +0100

    feat: add preliminary quote object

--- src/quote/index.ts
+++ src/quote/index.ts
@@ -1,7 +1,14 @@
+import { TransactionRequest } from '@ethersproject/abstract-provider'
 import { BigNumber } from '@ethersproject/bignumber'
 
 import { QuoteProvider } from './quoteProvider'
 import { QuoteToken } from './quoteToken'
+
+enum FlashMintContractType {
+  leveraged,
+  wrapped,
+  zeroEx,
+}
 
 export interface FlashMintQuoteRequest {
   isMinting: boolean
@@ -12,11 +19,16 @@ export interface FlashMintQuoteRequest {
 }
 
 export interface FlashMintQuote {
+  chainId: number
+  contractType: FlashMintContractType
+  contract: string
   isMinting: boolean
   inputToken: QuoteToken
   outputToken: QuoteToken
   indexTokenAmount: BigNumber
+  inputOutputAmount: BigNumber
   slippage: number
+  tx: TransactionRequest
 }
 
 export class FlashMintQuoteProvider
