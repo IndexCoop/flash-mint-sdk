883b0507dcfed61d2ceb3e8e4fe7d444ef643dc1
JD <jann.driessen@gmx.de>
2023-03-29 09:10:44 +0200

    test: fix tests for component swap data when redeeming

--- src/utils/componentSwapData.test.ts
+++ src/utils/componentSwapData.test.ts
@@ -76,66 +76,66 @@ describe('getIssuanceComponentSwapData()', () => {
       expect(swapData.dexData.pool).toBe(zeroAddress)
     })
   })
+})
 
-  describe('getRedemptionComponentSwapData()', () => {
-    test('returns correct swap data based on output token (USDC)', async () => {
-      const outputToken = usdc
-      const componentSwapData = await getRedemptionComponentSwapData(
-        MoneyMarketIndex.symbol,
-        MoneyMarketIndex.address!,
-        outputToken,
-        wei(1),
-        provider
-      )
-      expect(componentSwapData.length).toBe(6)
-      expect(componentSwapData[0].underlyingERC20).toBe(usdc)
-      expect(componentSwapData[1].underlyingERC20).toBe(usdt)
-      expect(componentSwapData[2].underlyingERC20).toBe(usdt)
-      expect(componentSwapData[3].underlyingERC20).toBe(dai)
-      expect(componentSwapData[4].underlyingERC20).toBe(dai)
-      expect(componentSwapData[5].underlyingERC20).toBe(usdc)
-      expect(componentSwapData[0].dexData.path).toEqual([usdc, weth, usdc])
-      expect(componentSwapData[1].dexData.path).toEqual([usdt, weth, usdc])
-      expect(componentSwapData[2].dexData.path).toEqual([usdt, weth, usdc])
-      expect(componentSwapData[3].dexData.path).toEqual([dai, weth, usdc])
-      expect(componentSwapData[4].dexData.path).toEqual([dai, weth, usdc])
-      expect(componentSwapData[5].dexData.path).toEqual([usdc, weth, usdc])
-      componentSwapData.forEach((swapData) => {
-        expect(swapData.buyUnderlyingAmount.eq(0)).toBe(true)
-        expect(swapData.dexData.exchange).toBe(Exchange.UniV3)
-        expect(swapData.dexData.fees).toEqual([3000, 3000])
-        expect(swapData.dexData.pool).toBe(zeroAddress)
-      })
+describe('getRedemptionComponentSwapData()', () => {
+  test('returns correct swap data based on output token (USDC)', async () => {
+    const outputToken = usdc
+    const componentSwapData = await getRedemptionComponentSwapData(
+      MoneyMarketIndex.symbol,
+      MoneyMarketIndex.address!,
+      outputToken,
+      wei(1),
+      provider
+    )
+    expect(componentSwapData.length).toBe(6)
+    expect(componentSwapData[0].underlyingERC20).toBe(usdc)
+    expect(componentSwapData[1].underlyingERC20).toBe(usdt)
+    expect(componentSwapData[2].underlyingERC20).toBe(usdt)
+    expect(componentSwapData[3].underlyingERC20).toBe(dai)
+    expect(componentSwapData[4].underlyingERC20).toBe(dai)
+    expect(componentSwapData[5].underlyingERC20).toBe(usdc)
+    expect(componentSwapData[0].dexData.path).toEqual([usdc, weth, usdc])
+    expect(componentSwapData[1].dexData.path).toEqual([usdt, weth, usdc])
+    expect(componentSwapData[2].dexData.path).toEqual([usdt, weth, usdc])
+    expect(componentSwapData[3].dexData.path).toEqual([dai, weth, usdc])
+    expect(componentSwapData[4].dexData.path).toEqual([dai, weth, usdc])
+    expect(componentSwapData[5].dexData.path).toEqual([usdc, weth, usdc])
+    componentSwapData.forEach((swapData) => {
+      expect(swapData.buyUnderlyingAmount.gt(0)).toBe(true)
+      expect(swapData.dexData.exchange).toBe(Exchange.UniV3)
+      expect(swapData.dexData.fees).toEqual([3000, 3000])
+      expect(swapData.dexData.pool).toBe(zeroAddress)
     })
+  })
 
-    test('returns correct swap data when output token is WETH', async () => {
-      const outputToken = weth
-      const componentSwapData = await getRedemptionComponentSwapData(
-        MoneyMarketIndex.symbol,
-        MoneyMarketIndex.address!,
-        outputToken,
-        wei(1),
-        provider
-      )
-      expect(componentSwapData.length).toBe(6)
-      expect(componentSwapData[0].underlyingERC20).toBe(usdc)
-      expect(componentSwapData[1].underlyingERC20).toBe(usdt)
-      expect(componentSwapData[2].underlyingERC20).toBe(usdt)
-      expect(componentSwapData[3].underlyingERC20).toBe(dai)
-      expect(componentSwapData[4].underlyingERC20).toBe(dai)
-      expect(componentSwapData[5].underlyingERC20).toBe(usdc)
-      expect(componentSwapData[0].dexData.path).toEqual([usdc, weth])
-      expect(componentSwapData[1].dexData.path).toEqual([usdt, weth])
-      expect(componentSwapData[2].dexData.path).toEqual([usdt, weth])
-      expect(componentSwapData[3].dexData.path).toEqual([dai, weth])
-      expect(componentSwapData[4].dexData.path).toEqual([dai, weth])
-      expect(componentSwapData[5].dexData.path).toEqual([usdc, weth])
-      componentSwapData.forEach((swapData) => {
-        expect(swapData.buyUnderlyingAmount.eq(0)).toBe(true)
-        expect(swapData.dexData.exchange).toBe(Exchange.UniV3)
-        expect(swapData.dexData.fees).toEqual([3000])
-        expect(swapData.dexData.pool).toBe(zeroAddress)
-      })
+  test('returns correct swap data when output token is WETH', async () => {
+    const outputToken = weth
+    const componentSwapData = await getRedemptionComponentSwapData(
+      MoneyMarketIndex.symbol,
+      MoneyMarketIndex.address!,
+      outputToken,
+      wei(1),
+      provider
+    )
+    expect(componentSwapData.length).toBe(6)
+    expect(componentSwapData[0].underlyingERC20).toBe(usdc)
+    expect(componentSwapData[1].underlyingERC20).toBe(usdt)
+    expect(componentSwapData[2].underlyingERC20).toBe(usdt)
+    expect(componentSwapData[3].underlyingERC20).toBe(dai)
+    expect(componentSwapData[4].underlyingERC20).toBe(dai)
+    expect(componentSwapData[5].underlyingERC20).toBe(usdc)
+    expect(componentSwapData[0].dexData.path).toEqual([usdc, weth])
+    expect(componentSwapData[1].dexData.path).toEqual([usdt, weth])
+    expect(componentSwapData[2].dexData.path).toEqual([usdt, weth])
+    expect(componentSwapData[3].dexData.path).toEqual([dai, weth])
+    expect(componentSwapData[4].dexData.path).toEqual([dai, weth])
+    expect(componentSwapData[5].dexData.path).toEqual([usdc, weth])
+    componentSwapData.forEach((swapData) => {
+      expect(swapData.buyUnderlyingAmount.gt(0)).toBe(true)
+      expect(swapData.dexData.exchange).toBe(Exchange.UniV3)
+      expect(swapData.dexData.fees).toEqual([3000])
+      expect(swapData.dexData.pool).toBe(zeroAddress)
     })
   })
 })
