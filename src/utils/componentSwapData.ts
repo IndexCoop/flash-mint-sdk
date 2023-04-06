475aecd807efac28b7d8c13218ed93873d69a231
SnakePoison <Ramysmelo@gmail.com>
2023-04-06 11:11:35 -0400

    fix: Refactor to use erc4626 convert calls
    There seems to have been a bug with the calculation for some components.
    
    This implementation will reliably work to calculate sharesToAssets and apply a pos or neg slippage

--- src/utils/componentSwapData.ts
+++ src/utils/componentSwapData.ts
@@ -33,10 +33,83 @@ const IssuanceAbi = [
   'function getRequiredComponentRedemptionUnits(address _setToken, uint256 _quantity) external view returns (address[] memory, uint256[] memory, uint256[] memory)',
 ]
 
+const erc4626Abi = [
+  'constructor(address _morpho, address _morphoToken, address _lens, address _recipient)',
+  'error ZeroAddress()',
+  'event Accrued(address indexed user, uint256 index, uint256 unclaimed)',
+  'event Approval(address indexed owner, address indexed spender, uint256 value)',
+  'event Claimed(address indexed user, uint256 claimed)',
+  'event Deposit(address indexed caller, address indexed owner, uint256 assets, uint256 shares)',
+  'event Initialized(uint8 version)',
+  'event OwnershipTransferred(address indexed previousOwner, address indexed newOwner)',
+  'event RewardsTransferred(address recipient, uint256 amount)',
+  'event Transfer(address indexed from, address indexed to, uint256 value)',
+  'event Withdraw(address indexed caller, address indexed receiver, address indexed owner, uint256 assets, uint256 shares)',
+  'function allowance(address owner, address spender) view returns (uint256)',
+  'function approve(address spender, uint256 amount) returns (bool)',
+  'function asset() view returns (address)',
+  'function balanceOf(address account) view returns (uint256)',
+  'function claimRewards(address _user) returns (uint256 rewardsAmount)',
+  'function comp() view returns (address)',
+  'function convertToAssets(uint256 shares) view returns (uint256 assets)',
+  'function convertToShares(uint256 assets) view returns (uint256 shares)',
+  'function decimals() view returns (uint8)',
+  'function decreaseAllowance(address spender, uint256 subtractedValue) returns (bool)',
+  'function deposit(uint256 assets, address receiver) returns (uint256)',
+  'function increaseAllowance(address spender, uint256 addedValue) returns (bool)',
+  'function initialize(address _poolToken, string _name, string _symbol, uint256 _initialDeposit)',
+  'function lens() view returns (address)',
+  'function maxDeposit(address) view returns (uint256)',
+  'function maxMint(address) view returns (uint256)',
+  'function maxRedeem(address owner) view returns (uint256)',
+  'function maxWithdraw(address owner) view returns (uint256)',
+  'function mint(uint256 shares, address receiver) returns (uint256)',
+  'function morpho() view returns (address)',
+  'function morphoToken() view returns (address)',
+  'function name() view returns (string)',
+  'function owner() view returns (address)',
+  'function poolToken() view returns (address)',
+  'function previewDeposit(uint256 assets) view returns (uint256)',
+  'function previewMint(uint256 shares) view returns (uint256)',
+  'function previewRedeem(uint256 shares) view returns (uint256)',
+  'function previewWithdraw(uint256 assets) view returns (uint256)',
+  'function recipient() view returns (address)',
+  'function redeem(uint256 shares, address receiver, address owner) returns (uint256)',
+  'function renounceOwnership()',
+  'function rewardsIndex() view returns (uint256)',
+  'function symbol() view returns (string)',
+  'function totalAssets() view returns (uint256)',
+  'function totalSupply() view returns (uint256)',
+  'function transfer(address to, uint256 amount) returns (bool)',
+  'function transferFrom(address from, address to, uint256 amount) returns (bool)',
+  'function transferOwnership(address newOwner)',
+  'function transferRewards()',
+  'function userRewards(address) view returns (uint128 index, uint128 unclaimed)',
+  'function wEth() view returns (address)',
+  'function withdraw(uint256 assets, address receiver, address owner) returns (uint256)',
+]
 const dai = DAI.address!
 const usdc = USDC.address!
 const usdt = USDT.address!
 const weth = WETH.address!
+
+const getAmountOfAssetToObtainShares = async (
+  component: string,
+  shares: BigNumber,
+  provider: JsonRpcProvider,
+  slippage = 0.15 // 1 = 1%
+) => {
+  const componentContract = new Contract(component, erc4626Abi, provider)
+  // Convert slippage to a BigNumber, do rounding to avoid weird JS precision loss
+  const slippageBigNumber = BigNumber.from(Math.round(slippage * 10000))
+
+  // Calculate the multiplier (1 + slippage)
+  const multiplier = BigNumber.from(10000).add(slippageBigNumber)
+
+  const buyUnderlyingAmount: BigNumber =
+    await componentContract.convertToAssets(shares)
+  return buyUnderlyingAmount.mul(multiplier).div(10000)
+}
 
 export async function getIssuanceComponentSwapData(
   indexTokenSymbol: string,
@@ -56,19 +129,16 @@ export async function getIssuanceComponentSwapData(
     issuanceComponents.map((component: string) =>
       getUnderlyingErc20(component, provider)
     )
+  const buyAmountsPromises = issuanceComponents.map(
+    (component: string, index: number) =>
+      getAmountOfAssetToObtainShares(component, issuanceUnits[index], provider)
+  )
+  const buyAmounts = await Promise.all(buyAmountsPromises)
   const wrappedTokens = await Promise.all(underlyingERC20sPromises)
   const swapData = issuanceComponents.map((_: string, index: number) => {
     const wrappedToken = wrappedTokens[index]
     const underlyingERC20 = wrappedToken.underlyingErc20
-    const buyUnderlyingAmount = fromWei(
-      issuanceUnits[index],
-      wrappedToken.decimals,
-      underlyingERC20.decimals
-    )
-      // FIXME: add slippage? Although right it seems to not work with slippage set
-      // in tests (0.5%)
-      .mul(103)
-      .div(100)
+    const buyUnderlyingAmount = buyAmounts[index]
     return {
       underlyingERC20: underlyingERC20.address,
       buyUnderlyingAmount,
@@ -97,18 +167,20 @@ export async function getRedemptionComponentSwapData(
       getUnderlyingErc20(component, provider)
     )
   const wrappedTokens = await Promise.all(underlyingERC20sPromises)
+  const buyAmountsPromises = issuanceComponents.map(
+    (component: string, index: number) =>
+      getAmountOfAssetToObtainShares(
+        component,
+        issuanceUnits[index],
+        provider,
+        -0.15
+      )
+  )
+  const buyAmounts = await Promise.all(buyAmountsPromises)
   const swapData = issuanceComponents.map((_: string, index: number) => {
     const wrappedToken = wrappedTokens[index]
     const underlyingERC20 = wrappedToken.underlyingErc20
-    let buyUnderlyingAmount = fromWei(
-      issuanceUnits[index],
-      wrappedToken.decimals,
-      underlyingERC20.decimals
-    )
-      // FIXME: add slippage? Although right it seems to not work with slippage set
-      // in tests (0.5%)
-      .mul(100)
-      .div(103)
+    const buyUnderlyingAmount = buyAmounts[index]
     return {
       underlyingERC20: underlyingERC20.address,
       buyUnderlyingAmount,
