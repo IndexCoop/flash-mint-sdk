b5f845563b105d58f315329204b9e4f807b1dd09
JD <jann.driessen@gmx.de>
2023-07-28 10:19:38 +0200

    test: pair redeems with mints to avoid redemption errors for exceeding icreth balance

--- src/tests/icreth.test.ts
+++ src/tests/icreth.test.ts
@@ -87,266 +87,274 @@ async function redeem(
 }
 
 describe('icRETH (mainnet) - ETH', () => {
-  let quote: Awaited<
-    ReturnType<typeof LeveragedQuoteProvider.prototype.getQuote>
-  >
-  const inputToken = eth
-  const isMinting = true
-  const indexTokenAmount = wei('1')
-  const quoteProvider = new LeveragedQuoteProvider(provider, zeroExApi)
-  beforeEach(async () => {
-    // Get quote
-    quote = await quoteProvider.getQuote({
-      inputToken,
-      outputToken: indexToken,
-      indexTokenAmount,
-      isMinting,
-      slippage,
+  describe('mint ETH', () => {
+    let quote: Awaited<
+      ReturnType<typeof LeveragedQuoteProvider.prototype.getQuote>
+    >
+    const inputToken = eth
+    const isMinting = true
+    const indexTokenAmount = wei('1')
+    const quoteProvider = new LeveragedQuoteProvider(provider, zeroExApi)
+    beforeEach(async () => {
+      // Get quote
+      quote = await quoteProvider.getQuote({
+        inputToken,
+        outputToken: indexToken,
+        indexTokenAmount,
+        isMinting,
+        slippage,
+      })
+    })
+
+    test('can mint icRETH from ETH', async () => {
+      if (!quote) throw new Error('No quote provided')
+      expect(quote).toBeDefined()
+      expect(quote.inputOutputTokenAmount.gt(0)).toBe(true)
+      expect(quote.indexTokenAmount).toEqual(indexTokenAmount)
+      expect(quote.swapDataDebtCollateral).toBeDefined()
+      expect(quote.swapDataPaymentToken).toBeDefined()
+      await mint(inputToken, indexTokenAmount, quote)
     })
   })
 
-  test('can mint icRETH from ETH', async () => {
-    if (!quote) throw new Error('No quote provided')
-    expect(quote).toBeDefined()
-    expect(quote.inputOutputTokenAmount.gt(0)).toBe(true)
-    expect(quote.indexTokenAmount).toEqual(indexTokenAmount)
-    expect(quote.swapDataDebtCollateral).toBeDefined()
-    expect(quote.swapDataPaymentToken).toBeDefined()
-    await mint(inputToken, indexTokenAmount, quote)
+  describe('redeem ETH', () => {
+    let quote: Awaited<
+      ReturnType<typeof LeveragedQuoteProvider.prototype.getQuote>
+    >
+    const outputToken = eth
+    const isMinting = false
+    const indexTokenAmount = wei('1')
+    const quoteProvider = new LeveragedQuoteProvider(provider, zeroExApi)
+    beforeEach(async () => {
+      // Get quote
+      quote = await quoteProvider.getQuote({
+        inputToken: indexToken,
+        outputToken,
+        indexTokenAmount,
+        isMinting,
+        slippage,
+      })
+    })
+
+    test('can redeem icRETH for ETH', async () => {
+      if (!quote) throw new Error('No quote provided')
+      expect(quote).toBeDefined()
+      expect(quote.inputOutputTokenAmount.gt(0)).toBe(true)
+      expect(quote.indexTokenAmount).toEqual(indexTokenAmount)
+      expect(quote.swapDataDebtCollateral).toBeDefined()
+      expect(quote.swapDataPaymentToken).toBeDefined()
+      await redeem(outputToken, indexTokenAmount, quote)
+    })
   })
 })
 
 describe('icRETH (mainnet) - rETH', () => {
-  const rethWhale = '0x7d6149aD9A573A6E2Ca6eBf7D4897c1B766841B4'
-  let quote: Awaited<
-    ReturnType<typeof LeveragedQuoteProvider.prototype.getQuote>
-  >
-  const inputToken = reth
-  const isMinting = true
-  const indexTokenAmount = wei('1')
-  const quoteProvider = new LeveragedQuoteProvider(provider, zeroExApi)
-  beforeAll(async () => {
-    await transferFromWhale(
-      rethWhale,
-      signer.address,
-      wei(100),
-      inputToken.address,
-      provider
-    )
-  })
-  beforeEach(async () => {
-    // Get quote
-    quote = await quoteProvider.getQuote({
-      inputToken,
-      outputToken: indexToken,
-      indexTokenAmount,
-      isMinting,
-      slippage,
+  describe('mint rETH', () => {
+    const rethWhale = '0x7d6149aD9A573A6E2Ca6eBf7D4897c1B766841B4'
+    let quote: Awaited<
+      ReturnType<typeof LeveragedQuoteProvider.prototype.getQuote>
+    >
+    const inputToken = reth
+    const isMinting = true
+    const indexTokenAmount = wei('1')
+    const quoteProvider = new LeveragedQuoteProvider(provider, zeroExApi)
+    beforeAll(async () => {
+      await transferFromWhale(
+        rethWhale,
+        signer.address,
+        wei(100),
+        inputToken.address,
+        provider
+      )
+    })
+    beforeEach(async () => {
+      // Get quote
+      quote = await quoteProvider.getQuote({
+        inputToken,
+        outputToken: indexToken,
+        indexTokenAmount,
+        isMinting,
+        slippage,
+      })
+    })
+
+    test('can mint icRETH from rETH', async () => {
+      if (!quote) throw new Error('No quote provided')
+      expect(quote).toBeDefined()
+      expect(quote.inputOutputTokenAmount.gt(0)).toBe(true)
+      expect(quote.indexTokenAmount).toEqual(indexTokenAmount)
+      expect(quote.swapDataDebtCollateral).toBeDefined()
+      expect(quote.swapDataPaymentToken).toBeDefined()
+      await mint(inputToken, indexTokenAmount, quote)
     })
   })
 
-  test('can mint icRETH from rETH', async () => {
-    if (!quote) throw new Error('No quote provided')
-    expect(quote).toBeDefined()
-    expect(quote.inputOutputTokenAmount.gt(0)).toBe(true)
-    expect(quote.indexTokenAmount).toEqual(indexTokenAmount)
-    expect(quote.swapDataDebtCollateral).toBeDefined()
-    expect(quote.swapDataPaymentToken).toBeDefined()
-    await mint(inputToken, indexTokenAmount, quote)
+  describe('redeem rETH', () => {
+    let quote: Awaited<
+      ReturnType<typeof LeveragedQuoteProvider.prototype.getQuote>
+    >
+    const outputToken = reth
+    const isMinting = false
+    const indexTokenAmount = wei('1')
+    const quoteProvider = new LeveragedQuoteProvider(provider, zeroExApi)
+    beforeEach(async () => {
+      // Get quote
+      quote = await quoteProvider.getQuote({
+        inputToken: indexToken,
+        outputToken,
+        indexTokenAmount,
+        isMinting,
+        slippage,
+      })
+    })
+
+    test('can redeem icRETH for rETH', async () => {
+      if (!quote) throw new Error('No quote provided')
+      expect(quote).toBeDefined()
+      expect(quote.inputOutputTokenAmount.gt(0)).toBe(true)
+      expect(quote.indexTokenAmount).toEqual(indexTokenAmount)
+      expect(quote.swapDataDebtCollateral).toBeDefined()
+      expect(quote.swapDataPaymentToken).toBeDefined()
+      await redeem(outputToken, indexTokenAmount, quote)
+    })
   })
 })
 
 describe('icRETH (mainnet) - USDC', () => {
-  const usdcWhale = '0x7713974908Be4BEd47172370115e8b1219F4A5f0'
-  let quote: Awaited<
-    ReturnType<typeof LeveragedQuoteProvider.prototype.getQuote>
-  >
-  const inputToken = usdc
-  const isMinting = true
-  const indexTokenAmount = wei('1')
-  const quoteProvider = new LeveragedQuoteProvider(provider, zeroExApi)
-  beforeAll(async () => {
-    await transferFromWhale(
-      usdcWhale,
-      signer.address,
-      wei(5000, 6),
-      inputToken.address,
-      provider
-    )
-  })
+  describe('mint USDC', () => {
+    const usdcWhale = '0x7713974908Be4BEd47172370115e8b1219F4A5f0'
+    let quote: Awaited<
+      ReturnType<typeof LeveragedQuoteProvider.prototype.getQuote>
+    >
+    const inputToken = usdc
+    const isMinting = true
+    const indexTokenAmount = wei('1')
+    const quoteProvider = new LeveragedQuoteProvider(provider, zeroExApi)
+    beforeAll(async () => {
+      await transferFromWhale(
+        usdcWhale,
+        signer.address,
+        wei(5000, 6),
+        inputToken.address,
+        provider
+      )
+    })
 
-  beforeEach(async () => {
-    // Get quote
-    quote = await quoteProvider.getQuote({
-      inputToken,
-      outputToken: indexToken,
-      indexTokenAmount,
-      isMinting,
-      slippage,
+    beforeEach(async () => {
+      // Get quote
+      quote = await quoteProvider.getQuote({
+        inputToken,
+        outputToken: indexToken,
+        indexTokenAmount,
+        isMinting,
+        slippage,
+      })
+    })
+
+    test('can mint icRETH from USDC', async () => {
+      if (!quote) throw new Error('No quote provided')
+      expect(quote).toBeDefined()
+      expect(quote.inputOutputTokenAmount.gt(0)).toBe(true)
+      expect(quote.indexTokenAmount).toEqual(indexTokenAmount)
+      expect(quote.swapDataDebtCollateral).toBeDefined()
+      expect(quote.swapDataPaymentToken).toBeDefined()
+      await mint(inputToken, indexTokenAmount, quote)
     })
   })
 
-  test('can mint icRETH from USDC', async () => {
-    if (!quote) throw new Error('No quote provided')
-    expect(quote).toBeDefined()
-    expect(quote.inputOutputTokenAmount.gt(0)).toBe(true)
-    expect(quote.indexTokenAmount).toEqual(indexTokenAmount)
-    expect(quote.swapDataDebtCollateral).toBeDefined()
-    expect(quote.swapDataPaymentToken).toBeDefined()
-    await mint(inputToken, indexTokenAmount, quote)
+  describe('redeem USDC', () => {
+    let quote: Awaited<
+      ReturnType<typeof LeveragedQuoteProvider.prototype.getQuote>
+    >
+    const outputToken = usdc
+    const isMinting = false
+    const indexTokenAmount = wei('1')
+    const quoteProvider = new LeveragedQuoteProvider(provider, zeroExApi)
+    beforeEach(async () => {
+      // Get quote
+      quote = await quoteProvider.getQuote({
+        inputToken: indexToken,
+        outputToken,
+        indexTokenAmount,
+        isMinting,
+        slippage,
+      })
+    })
+
+    test('can redeem icRETH for USDC', async () => {
+      if (!quote) throw new Error('No quote provided')
+      expect(quote).toBeDefined()
+      expect(quote.inputOutputTokenAmount.gt(0)).toBe(true)
+      expect(quote.indexTokenAmount).toEqual(indexTokenAmount)
+      expect(quote.swapDataDebtCollateral).toBeDefined()
+      expect(quote.swapDataPaymentToken).toBeDefined()
+      await redeem(outputToken, indexTokenAmount, quote)
+    })
   })
 })
 
 describe('icRETH (mainnet) - WETH', () => {
-  let quote: Awaited<
-    ReturnType<typeof LeveragedQuoteProvider.prototype.getQuote>
-  >
-  const inputToken = weth
-  const isMinting = true
-  const indexTokenAmount = wei('1')
-  const quoteProvider = new LeveragedQuoteProvider(provider, zeroExApi)
-  beforeAll(async () => {
-    await wrapETH(wei(2), signer)
-  })
+  describe('mint WETH', () => {
+    let quote: Awaited<
+      ReturnType<typeof LeveragedQuoteProvider.prototype.getQuote>
+    >
+    const inputToken = weth
+    const isMinting = true
+    const indexTokenAmount = wei('1')
+    const quoteProvider = new LeveragedQuoteProvider(provider, zeroExApi)
+    beforeAll(async () => {
+      await wrapETH(wei(2), signer)
+    })
 
-  beforeEach(async () => {
-    // Get quote
-    quote = await quoteProvider.getQuote({
-      inputToken,
-      outputToken: indexToken,
-      indexTokenAmount,
-      isMinting,
-      slippage,
+    beforeEach(async () => {
+      // Get quote
+      quote = await quoteProvider.getQuote({
+        inputToken,
+        outputToken: indexToken,
+        indexTokenAmount,
+        isMinting,
+        slippage,
+      })
+    })
+
+    test('can mint icRETH from WETH', async () => {
+      if (!quote) throw new Error('No quote provided')
+      expect(quote).toBeDefined()
+      expect(quote.inputOutputTokenAmount.gt(0)).toBe(true)
+      expect(quote.indexTokenAmount).toEqual(indexTokenAmount)
+      expect(quote.swapDataDebtCollateral).toBeDefined()
+      expect(quote.swapDataPaymentToken).toBeDefined()
+      await mint(inputToken, indexTokenAmount, quote)
     })
   })
 
-  test('can mint icRETH from WETH', async () => {
-    if (!quote) throw new Error('No quote provided')
-    expect(quote).toBeDefined()
-    expect(quote.inputOutputTokenAmount.gt(0)).toBe(true)
-    expect(quote.indexTokenAmount).toEqual(indexTokenAmount)
-    expect(quote.swapDataDebtCollateral).toBeDefined()
-    expect(quote.swapDataPaymentToken).toBeDefined()
-    await mint(inputToken, indexTokenAmount, quote)
-  })
-})
-
-describe('icRETH (mainnet) - redeem ETH', () => {
-  let quote: Awaited<
-    ReturnType<typeof LeveragedQuoteProvider.prototype.getQuote>
-  >
-  const outputToken = eth
-  const isMinting = false
-  const indexTokenAmount = wei('1')
-  const quoteProvider = new LeveragedQuoteProvider(provider, zeroExApi)
-  beforeEach(async () => {
-    // Get quote
-    quote = await quoteProvider.getQuote({
-      inputToken: indexToken,
-      outputToken,
-      indexTokenAmount,
-      isMinting,
-      slippage,
+  describe('redeem WETH', () => {
+    let quote: Awaited<
+      ReturnType<typeof LeveragedQuoteProvider.prototype.getQuote>
+    >
+    const outputToken = weth
+    const isMinting = false
+    const indexTokenAmount = wei('1')
+    const quoteProvider = new LeveragedQuoteProvider(provider, zeroExApi)
+    beforeEach(async () => {
+      // Get quote
+      quote = await quoteProvider.getQuote({
+        inputToken: indexToken,
+        outputToken,
+        indexTokenAmount,
+        isMinting,
+        slippage,
+      })
     })
-  })
 
-  test('can redeem icRETH for ETH', async () => {
-    if (!quote) throw new Error('No quote provided')
-    expect(quote).toBeDefined()
-    expect(quote.inputOutputTokenAmount.gt(0)).toBe(true)
-    expect(quote.indexTokenAmount).toEqual(indexTokenAmount)
-    expect(quote.swapDataDebtCollateral).toBeDefined()
-    expect(quote.swapDataPaymentToken).toBeDefined()
-    await redeem(outputToken, indexTokenAmount, quote)
-  })
-})
-
-describe('icRETH (mainnet) - redeem rETH', () => {
-  let quote: Awaited<
-    ReturnType<typeof LeveragedQuoteProvider.prototype.getQuote>
-  >
-  const outputToken = reth
-  const isMinting = false
-  const indexTokenAmount = wei('1')
-  const quoteProvider = new LeveragedQuoteProvider(provider, zeroExApi)
-  beforeEach(async () => {
-    // Get quote
-    quote = await quoteProvider.getQuote({
-      inputToken: indexToken,
-      outputToken,
-      indexTokenAmount,
-      isMinting,
-      slippage,
+    test('can redeem icRETH for WETH', async () => {
+      if (!quote) throw new Error('No quote provided')
+      expect(quote).toBeDefined()
+      expect(quote.inputOutputTokenAmount.gt(0)).toBe(true)
+      expect(quote.indexTokenAmount).toEqual(indexTokenAmount)
+      expect(quote.swapDataDebtCollateral).toBeDefined()
+      expect(quote.swapDataPaymentToken).toBeDefined()
+      await redeem(outputToken, indexTokenAmount, quote)
     })
-  })
-
-  test('can redeem icRETH for rETH', async () => {
-    if (!quote) throw new Error('No quote provided')
-    expect(quote).toBeDefined()
-    expect(quote.inputOutputTokenAmount.gt(0)).toBe(true)
-    expect(quote.indexTokenAmount).toEqual(indexTokenAmount)
-    expect(quote.swapDataDebtCollateral).toBeDefined()
-    expect(quote.swapDataPaymentToken).toBeDefined()
-    await redeem(outputToken, indexTokenAmount, quote)
-  })
-})
-
-describe('icRETH (mainnet) - redeem USDC', () => {
-  let quote: Awaited<
-    ReturnType<typeof LeveragedQuoteProvider.prototype.getQuote>
-  >
-  const outputToken = usdc
-  const isMinting = false
-  const indexTokenAmount = wei('1')
-  const quoteProvider = new LeveragedQuoteProvider(provider, zeroExApi)
-  beforeEach(async () => {
-    // Get quote
-    quote = await quoteProvider.getQuote({
-      inputToken: indexToken,
-      outputToken,
-      indexTokenAmount,
-      isMinting,
-      slippage,
-    })
-  })
-
-  test('can redeem icRETH for USDC', async () => {
-    if (!quote) throw new Error('No quote provided')
-    expect(quote).toBeDefined()
-    expect(quote.inputOutputTokenAmount.gt(0)).toBe(true)
-    expect(quote.indexTokenAmount).toEqual(indexTokenAmount)
-    expect(quote.swapDataDebtCollateral).toBeDefined()
-    expect(quote.swapDataPaymentToken).toBeDefined()
-    await redeem(outputToken, indexTokenAmount, quote)
-  })
-})
-
-describe('icRETH (mainnet) - redeem WETH', () => {
-  let quote: Awaited<
-    ReturnType<typeof LeveragedQuoteProvider.prototype.getQuote>
-  >
-  const outputToken = weth
-  const isMinting = false
-  const indexTokenAmount = wei('1')
-  const quoteProvider = new LeveragedQuoteProvider(provider, zeroExApi)
-  beforeEach(async () => {
-    // Get quote
-    quote = await quoteProvider.getQuote({
-      inputToken: indexToken,
-      outputToken,
-      indexTokenAmount,
-      isMinting,
-      slippage,
-    })
-  })
-
-  test('can redeem icRETH for WETH', async () => {
-    if (!quote) throw new Error('No quote provided')
-    expect(quote).toBeDefined()
-    expect(quote.inputOutputTokenAmount.gt(0)).toBe(true)
-    expect(quote.indexTokenAmount).toEqual(indexTokenAmount)
-    expect(quote.swapDataDebtCollateral).toBeDefined()
-    expect(quote.swapDataPaymentToken).toBeDefined()
-    await redeem(outputToken, indexTokenAmount, quote)
   })
 })
