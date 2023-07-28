2be4bed7ed6eb079b10ce089faf9a6dc9273fed4
snakepoison <ramy@indexcoop.com>
2023-07-28 13:51:56 +0200

    refactor icreth test.

--- src/tests/icreth.test.ts
+++ src/tests/icreth.test.ts
@@ -15,15 +15,12 @@ import {
   approveErc20,
 } from './utils'
 
-const slippage = 0.1
-
 const provider = LocalhostProvider
 const signer = SignerAccount2
 const zeroExApi = ZeroExApiSwapQuote
 
 // FIXME: add tests for all tokens (mint + redeem)
 const { eth, icreth, reth, usdc, weth } = QuoteTokens
-const indexToken = icreth
 
 const stealTokens = async (
   token: string,
@@ -43,59 +40,265 @@ const stealTokens = async (
   await provider.send('hardhat_stopImpersonatingAccount', [fromWhale])
 }
 describe('icRETH (mainnet)', () => {
-  const rethWhale = '0x7d6149aD9A573A6E2Ca6eBf7D4897c1B766841B4'
-  let quote: Awaited<
-    ReturnType<typeof LeveragedQuoteProvider.prototype.getQuote>
-  >
-  const inputToken = reth
-  const isMinting = true
-  const indexTokenAmount = wei('1')
-  const quoteProvider = new LeveragedQuoteProvider(provider, zeroExApi)
-  beforeAll(async () => {
-    await stealTokens(inputToken.address, wei(100), rethWhale)
-  })
-  beforeEach(async () => {
-    // Get quote
-    quote = await quoteProvider.getQuote({
-      inputToken,
-      outputToken: indexToken,
-      indexTokenAmount,
-      isMinting,
-      slippage,
-    })
-  })
+  describe('given a network provider, quote provider and tx builder', () => {
+    // Depends on a quote provider to be defined, but we don't know the type of a quote provider
+    type QuoterConfig<T extends typeof quoteProvider> = Parameters<
+      T['getQuote']
+    >[0]
+    type QuoteConfig = QuoterConfig<typeof quoteProvider>
 
-  test('can quote icRETH from rETH', async () => {
-    if (!quote) throw new Error('No quote provided')
-    expect(quote).toBeDefined()
-    expect(quote.inputOutputTokenAmount.gt(0)).toBe(true)
-    expect(quote.indexTokenAmount).toEqual(indexTokenAmount)
-    expect(quote.swapDataDebtCollateral).toBeDefined()
-    expect(quote.swapDataPaymentToken).toBeDefined()
-  })
+    const quoteProvider = new LeveragedQuoteProvider(provider, zeroExApi)
+    const txBuilder = new LeveragedTransactionBuilder(provider)
+    const configureTXFactory = <T extends typeof quoteProvider>(
+      quoter: T,
+      builderFactory: typeof txBuilder
+    ) => {
+      const quote = async (config: QuoterConfig<T>) => {
+        const result = await quoter.getQuote(config)
+        if (!result)
+          throw new Error(
+            'No quote provided for config: \n' +
+              JSON.stringify(config, null, 2) +
+              '\n'
+          )
 
-  test('can mint icRETH from rETH', async () => {
-    const balanceBefore: BigNumber = await balanceOf(signer, indexToken.address)
-    const builder = new LeveragedTransactionBuilder(provider)
-    if (!quote) throw new Error('No quote provided')
-    const tx = await builder.build({
-      isMinting,
-      indexToken: indexToken.address,
-      indexTokenSymbol: indexToken.symbol,
-      inputOutputToken: inputToken.address,
-      inputOutputTokenSymbol: inputToken.symbol,
-      indexTokenAmount,
-      inputOutputTokenAmount: quote.inputOutputTokenAmount,
-      swapDataDebtCollateral: quote.swapDataDebtCollateral,
-      swapDataPaymentToken: quote.swapDataPaymentToken,
+        return { config, result }
+      }
+      const build = async (quoted: Awaited<ReturnType<typeof quote>>) => {
+        const {
+          indexTokenAmount,
+          inputOutputTokenAmount,
+          swapDataDebtCollateral,
+          swapDataPaymentToken,
+        } = quoted.result
+
+        const config = quoted.config
+        const { isMinting, outputToken, inputToken } = config
+        const tx = await builderFactory.build({
+          isMinting,
+          indexToken: isMinting ? outputToken.address : inputToken.address,
+          indexTokenSymbol: isMinting ? outputToken.symbol : inputToken.symbol,
+          inputOutputToken: isMinting
+            ? inputToken.address
+            : outputToken.address,
+          inputOutputTokenSymbol: isMinting
+            ? inputToken.symbol
+            : outputToken.symbol,
+          indexTokenAmount,
+          inputOutputTokenAmount,
+          swapDataDebtCollateral,
+          swapDataPaymentToken,
+        })
+        if (!tx || !tx.to) throw new Error('No tx could be built')
+        const approveTX = async () => {
+          if (!tx.to) throw new Error('No tx could be built')
+          if (config.inputToken.address !== eth.address) {
+            await approveErc20(
+              config.inputToken.address,
+              tx.to,
+              isMinting ? inputOutputTokenAmount : indexTokenAmount,
+              signer
+            )
+          }
+        }
+        return { tx, approveTX }
+      }
+      return { quote, build }
+    }
+    const factory = configureTXFactory(quoteProvider, txBuilder)
+
+    describe('given the signer has eth, weth, reth and usdc', () => {
+      beforeAll(async () => {
+        const rethWhale = '0x7d6149aD9A573A6E2Ca6eBf7D4897c1B766841B4'
+        const wethWhale = '0xe5F8086DAc91E039b1400febF0aB33ba3487F29A'
+        const usdcWhale = '0x51eDF02152EBfb338e03E30d65C15fBf06cc9ECC'
+        await stealTokens(weth.address, wei(100), wethWhale)
+        await stealTokens(reth.address, wei(100), rethWhale)
+        await stealTokens(usdc.address, wei(100_000, 6), usdcWhale)
+      })
+      describe('reth mint and redeem', () => {
+        const mintQuoteConfig: QuoteConfig = {
+          inputToken: reth,
+          outputToken: icreth,
+          indexTokenAmount: wei('1'),
+          isMinting: true,
+          slippage: 0.1,
+        }
+        const redeemQuoteConfig: QuoteConfig = {
+          inputToken: icreth,
+          outputToken: reth,
+          indexTokenAmount: wei('1'),
+          isMinting: false,
+          slippage: 0.1,
+        }
+
+        test('can quote icRETH from rEth', async () => {
+          const { result: quote, config } = await factory.quote(mintQuoteConfig)
+          expect(quote.inputOutputTokenAmount.gt(0)).toBe(true)
+          expect(quote.indexTokenAmount).toEqual(config.indexTokenAmount)
+          expect(quote.swapDataDebtCollateral).toBeDefined()
+          expect(quote.swapDataPaymentToken).toBeDefined()
+        })
+
+        test('can mint icRETH from rEth', async () => {
+          const quote = await factory.quote(mintQuoteConfig)
+          const { tx, approveTX } = await factory.build(quote)
+          await approveTX()
+          const balanceBefore: BigNumber = await balanceOf(
+            signer,
+            quote.config.outputToken.address
+          )
+          const gasEstimate = await signer.estimateGas(tx)
+          tx.gasLimit = gasEstimate
+          const res = await signer.sendTransaction(tx)
+          await res.wait()
+          const balanceAfter: BigNumber = await balanceOf(
+            signer,
+            quote.config.outputToken.address
+          )
+          expect(
+            balanceAfter.gte(balanceBefore.add(quote.config.indexTokenAmount))
+          ).toBe(true)
+        })
+        describe('given a minted icRETH', () => {
+          beforeAll(async () => {
+            const { tx, approveTX } = await factory.build(
+              await factory.quote(mintQuoteConfig)
+            )
+            await approveTX()
+            const gasEstimate = await signer.estimateGas(tx)
+            tx.gasLimit = gasEstimate
+            const res = await signer.sendTransaction(tx)
+            await res.wait()
+          })
+
+          test('can quote rETH from icRETH', async () => {
+            const { result: quote, config } = await factory.quote(
+              redeemQuoteConfig
+            )
+            expect(quote.inputOutputTokenAmount.gt(0)).toBe(true)
+            expect(quote.indexTokenAmount).toEqual(config.indexTokenAmount)
+            expect(quote.swapDataDebtCollateral).toBeDefined()
+            expect(quote.swapDataPaymentToken).toBeDefined()
+          })
+
+          test('can redeem rETH from icRETH', async () => {
+            const quote = await factory.quote(redeemQuoteConfig)
+            const { tx, approveTX } = await factory.build(quote)
+
+            const balanceBefore: BigNumber = await balanceOf(
+              signer,
+              quote.config.outputToken.address
+            )
+            await approveTX()
+            const gasEstimate = await signer.estimateGas(tx)
+            tx.gasLimit = gasEstimate
+            const res = await signer.sendTransaction(tx)
+            await res.wait()
+            const balanceAfter: BigNumber = await balanceOf(
+              signer,
+              redeemQuoteConfig.outputToken.address
+            )
+            expect(
+              balanceAfter.gte(
+                balanceBefore.add(quote.result.inputOutputTokenAmount)
+              )
+            ).toBe(true)
+          })
+        })
+      })
+
+      describe('usdc mint and redeem', () => {
+        const mintQuoteConfig: QuoteConfig = {
+          inputToken: usdc,
+          outputToken: icreth,
+          indexTokenAmount: wei('1'),
+          isMinting: true,
+          slippage: 0.1,
+        }
+        const redeemQuoteConfig: QuoteConfig = {
+          inputToken: icreth,
+          outputToken: usdc,
+          indexTokenAmount: wei('1'),
+          isMinting: false,
+          slippage: 0.1,
+        }
+
+        test('can quote icRETH from usdc', async () => {
+          const { result: quote, config } = await factory.quote(mintQuoteConfig)
+          expect(quote.inputOutputTokenAmount.gt(0)).toBe(true)
+          expect(quote.indexTokenAmount).toEqual(config.indexTokenAmount)
+          expect(quote.swapDataDebtCollateral).toBeDefined()
+          expect(quote.swapDataPaymentToken).toBeDefined()
+        })
+
+        test('can mint icRETH from usdc', async () => {
+          const quote = await factory.quote(mintQuoteConfig)
+          const { tx, approveTX } = await factory.build(quote)
+          await approveTX()
+          const balanceBefore: BigNumber = await balanceOf(
+            signer,
+            quote.config.outputToken.address
+          )
+          const gasEstimate = await signer.estimateGas(tx)
+          tx.gasLimit = gasEstimate
+          const res = await signer.sendTransaction(tx)
+          await res.wait()
+          const balanceAfter: BigNumber = await balanceOf(
+            signer,
+            quote.config.outputToken.address
+          )
+          expect(
+            balanceAfter.gte(balanceBefore.add(quote.config.indexTokenAmount))
+          ).toBe(true)
+        })
+        describe('given a minted icRETH', () => {
+          beforeAll(async () => {
+            const { tx, approveTX } = await factory.build(
+              await factory.quote(mintQuoteConfig)
+            )
+            await approveTX()
+            const gasEstimate = await signer.estimateGas(tx)
+            tx.gasLimit = gasEstimate
+            const res = await signer.sendTransaction(tx)
+            await res.wait()
+          })
+
+          test('can quote usdc from icRETH', async () => {
+            const { result: quote, config } = await factory.quote(
+              redeemQuoteConfig
+            )
+            expect(quote.inputOutputTokenAmount.gt(0)).toBe(true)
+            expect(quote.indexTokenAmount).toEqual(config.indexTokenAmount)
+            expect(quote.swapDataDebtCollateral).toBeDefined()
+            expect(quote.swapDataPaymentToken).toBeDefined()
+          })
+
+          test('can redeem usdc from icRETH', async () => {
+            const quote = await factory.quote(redeemQuoteConfig)
+            const { tx, approveTX } = await factory.build(quote)
+
+            const balanceBefore: BigNumber = await balanceOf(
+              signer,
+              quote.config.outputToken.address
+            )
+            await approveTX()
+            const gasEstimate = await signer.estimateGas(tx)
+            tx.gasLimit = gasEstimate
+            const res = await signer.sendTransaction(tx)
+            await res.wait()
+            const balanceAfter: BigNumber = await balanceOf(
+              signer,
+              redeemQuoteConfig.outputToken.address
+            )
+            expect(
+              balanceAfter.gte(
+                balanceBefore.add(quote.result.inputOutputTokenAmount)
+              )
+            ).toBe(true)
+          })
+        })
+      })
     })
-    if (!tx || !tx.to) fail()
-    await approveErc20(inputToken.address, tx.to, wei(100), signer)
-    const gasEstimate = await signer.estimateGas(tx)
-    tx.gasLimit = gasEstimate
-    const res = await signer.sendTransaction(tx)
-    await res.wait()
-    const balanceAfter: BigNumber = await balanceOf(signer, indexToken.address)
-    expect(balanceAfter.gte(balanceBefore.add(indexTokenAmount))).toBe(true)
   })
 })
