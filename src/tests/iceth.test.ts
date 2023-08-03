76cdbeccb08de7677807c128a07281072f126c09
JD <jann.driessen@gmx.de>
2023-08-03 10:34:27 +0200

    test: update iceth tests

--- src/tests/iceth.test.ts
+++ src/tests/iceth.test.ts
@@ -1,123 +1,44 @@
 import { BigNumber } from '@ethersproject/bignumber'
 
-import { LeveragedTransactionBuilder } from 'flashmint/builders'
-import { LeveragedQuoteProvider } from 'quote/leveraged'
-import { wei } from 'utils/numbers'
-
 import {
-  approveErc20,
-  balanceOf,
   LocalhostProvider,
   QuoteTokens,
   SignerAccount4,
+  TestFactory,
+  wei,
   ZeroExApiSwapQuote,
 } from './utils'
 
 const { eth, iceth } = QuoteTokens
-
-const setToken = iceth
-const setTokenAddress = iceth.address
 const zeroExApi = ZeroExApiSwapQuote
-const provider = LocalhostProvider
-const signer = SignerAccount4
 
 describe('icETH (mainnet)', () => {
-  beforeEach((): void => {
-    jest.setTimeout(10000000)
+  let factory: TestFactory
+  beforeEach(async () => {
+    const provider = LocalhostProvider
+    const signer = SignerAccount4
+    factory = new TestFactory(provider, signer, zeroExApi)
   })
 
   test('can mint icETH-ETH', async () => {
-    const isMinting = true
-    const indexTokenAmount = wei('1')
-    const slippage = 0.5
-    // Get quote
-    const quoteProvider = new LeveragedQuoteProvider(provider, zeroExApi)
-    const quote = await quoteProvider.getQuote({
+    await factory.fetchQuote({
+      isMinting: true,
       inputToken: eth,
-      outputToken: setToken,
-      indexTokenAmount,
-      isMinting,
-      slippage,
+      outputToken: iceth,
+      indexTokenAmount: wei('1'),
+      slippage: 0.5,
     })
-    if (!quote) fail()
-    expect(quote).toBeDefined()
-    expect(quote.inputOutputTokenAmount.gt(0)).toBe(true)
-    expect(quote.indexTokenAmount).toEqual(indexTokenAmount)
-    expect(quote.swapDataDebtCollateral).toBeDefined()
-    expect(quote.swapDataPaymentToken).toBeDefined()
-
-    const builder = new LeveragedTransactionBuilder(provider)
-    const tx = await builder.build({
-      isMinting,
-      indexToken: setToken.address,
-      indexTokenSymbol: setToken.symbol,
-      inputOutputToken: eth.address,
-      inputOutputTokenSymbol: eth.symbol,
-      indexTokenAmount,
-      inputOutputTokenAmount: quote.inputOutputTokenAmount,
-      swapDataDebtCollateral: quote.swapDataDebtCollateral,
-      swapDataPaymentToken: quote.swapDataPaymentToken,
-    })
-    if (!tx) fail()
-    const balanceBefore = await balanceOf(signer, setTokenAddress)
-    const gasEstimate = await provider.estimateGas(tx)
-    tx.gasLimit = gasEstimate
-    const res = await signer.sendTransaction(tx)
-    res.wait()
-    const balanceAfter = await balanceOf(signer, setTokenAddress)
-    expect(balanceAfter.gte(balanceBefore.add(indexTokenAmount))).toBe(true)
+    await factory.executeTx()
   })
 
-  test('can redeem ETH', async () => {
-    const isMinting = false
-    const indexTokenAmount = wei('1')
-    const slippage = 0.5
-    // Get quote
-    const quoteProvider = new LeveragedQuoteProvider(provider, zeroExApi)
-    const quote = await quoteProvider.getQuote({
-      inputToken: setToken,
+  test('can redeem for ETH', async () => {
+    await factory.fetchQuote({
+      isMinting: false,
+      inputToken: iceth,
       outputToken: eth,
-      indexTokenAmount,
-      isMinting,
-      slippage,
+      indexTokenAmount: wei('1'),
+      slippage: 0.5,
     })
-    if (!quote) fail()
-    expect(quote).toBeDefined()
-    expect(quote.inputOutputTokenAmount.gt(0)).toBe(true)
-    expect(quote.indexTokenAmount).toEqual(indexTokenAmount)
-    expect(quote.swapDataDebtCollateral).toBeDefined()
-    expect(quote.swapDataPaymentToken).toBeDefined()
-
-    const builder = new LeveragedTransactionBuilder(provider)
-    const tx = await builder.build({
-      isMinting,
-      indexToken: setToken.address,
-      indexTokenSymbol: setToken.symbol,
-      inputOutputToken: eth.address,
-      inputOutputTokenSymbol: eth.symbol,
-      indexTokenAmount,
-      inputOutputTokenAmount: quote.inputOutputTokenAmount,
-      swapDataDebtCollateral: quote.swapDataDebtCollateral,
-      swapDataPaymentToken: quote.swapDataPaymentToken,
-    })
-    if (!tx) fail()
-    const balanceBefore = await balanceOf(signer, setTokenAddress)
-    approveErc20(
-      setTokenAddress,
-      /* eslint-disable  @typescript-eslint/no-non-null-assertion */
-      tx.to!,
-      indexTokenAmount,
-      signer
-    )
-    // Gas estimation fails but tx goes through
-    // const gasEstimate = await provider.estimateGas(tx)
-    // tx.gasLimit = gasEstimate
-    tx.gasLimit = 5_000_000
-    const nonce = (await signer.getTransactionCount()) + 1
-    tx.nonce = BigNumber.from(nonce)
-    const res = await signer.sendTransaction(tx)
-    res.wait()
-    const balanceAfter = await balanceOf(signer, setTokenAddress)
-    expect(balanceAfter.lte(balanceBefore.sub(indexTokenAmount))).toBe(true)
+    await factory.executeTx(BigNumber.from(5_000_000))
   })
 })
