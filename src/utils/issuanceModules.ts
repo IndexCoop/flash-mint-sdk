c066ac7735bad9308c7cc68b96f362431d70a486
JD <jann.driessen@gmx.de>
2023-10-20 12:10:19 -0400

    refactor: get issuance module

--- src/utils/issuanceModules.ts
+++ src/utils/issuanceModules.ts
@@ -24,53 +24,35 @@ export interface IssuanceModule {
   isDebtIssuance: boolean
 }
 
-function getIndexEthIssuanceModule(tokenSymbol: string): IssuanceModule {
+export function getIssuanceModule(
+  tokenSymbol: string,
+  chainId: number = ChainId.Mainnet
+): IssuanceModule {
+  if (chainId === ChainId.Polygon)
+    return {
+      address: BasicIssuanceModulePolygonAddress,
+      isDebtIssuance: false,
+    }
+
+  // Mainnet
   switch (tokenSymbol) {
+    case BTC2xFlexibleLeverageIndex.symbol:
+    case ETH2xFlexibleLeverageIndex.symbol:
+      return { address: DebtIssuanceModuleAddress, isDebtIssuance: true }
     case CoinDeskEthTrendIndex.symbol:
     case LeveragedrEthStakingYield.symbol:
       return {
         address: IndexDebtIssuanceModuleV2Address_v2,
         isDebtIssuance: true,
       }
-    default:
-      return { address: IndexDebtIssuanceModuleV2Address, isDebtIssuance: true }
-  }
-}
-
-function getEthIssuanceModuleAddress(tokenSymbol: string): IssuanceModule {
-  switch (tokenSymbol) {
-    case CoinDeskEthTrendIndex.symbol:
     case DiversifiedStakedETHIndex.symbol:
     case GitcoinStakedETHIndex.symbol:
-    case LeveragedrEthStakingYield.symbol:
     case MoneyMarketIndexToken.symbol:
     case wsETH2.symbol:
-      return getIndexEthIssuanceModule(tokenSymbol)
-    case BTC2xFlexibleLeverageIndex.symbol:
-    case ETH2xFlexibleLeverageIndex.symbol:
-      return { address: DebtIssuanceModuleAddress, isDebtIssuance: true }
+      return { address: IndexDebtIssuanceModuleV2Address, isDebtIssuance: true }
     case InterestCompoundingETHIndex.symbol:
       return { address: DebtIssuanceModuleV2Address, isDebtIssuance: true }
     default:
       return { address: BasicIssuanceModuleAddress, isDebtIssuance: false }
   }
-}
-
-function getPolygonIssuanceModuleAddress(tokenSymbol: string): IssuanceModule {
-  switch (tokenSymbol) {
-    default:
-      return {
-        address: BasicIssuanceModulePolygonAddress,
-        isDebtIssuance: false,
-      }
-  }
-}
-
-export function getIssuanceModule(
-  tokenSymbol: string,
-  chainId: number = ChainId.Mainnet
-): IssuanceModule {
-  return chainId === ChainId.Polygon
-    ? getPolygonIssuanceModuleAddress(tokenSymbol)
-    : getEthIssuanceModuleAddress(tokenSymbol)
 }
