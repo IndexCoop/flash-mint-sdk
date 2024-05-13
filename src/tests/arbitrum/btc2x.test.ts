6174e6d30d8332bb2b04f1265643a6f2e26614da
JD <jann.driessen@gmx.de>
2024-05-13 10:35:32 +0100

    test: add btc2x tests

---
+++ src/tests/arbitrum/btc2x.test.ts
@@ -0,0 +1,47 @@
+import { IndexCoopBitcoin2xIndex } from 'constants/tokens'
+import {
+  getSignerAccount,
+  LocalhostProviderArbitrum,
+  QuoteTokens,
+  TestFactory,
+  wei,
+  ZeroExApiArbitrumSwapQuote,
+} from 'tests/utils'
+
+const { eth } = QuoteTokens
+const btc2x = {
+  address: IndexCoopBitcoin2xIndex.addressArbitrum!,
+  decimals: 18,
+  symbol: IndexCoopBitcoin2xIndex.symbol,
+}
+
+describe('BTC2X (Arbitrum)', () => {
+  let factory: TestFactory
+  beforeEach(async () => {
+    const provider = LocalhostProviderArbitrum
+    const signer = getSignerAccount(3, provider)
+    factory = new TestFactory(provider, signer, ZeroExApiArbitrumSwapQuote)
+  })
+
+  test('can mint with ETH', async () => {
+    await factory.fetchQuote({
+      isMinting: true,
+      inputToken: eth,
+      outputToken: btc2x,
+      indexTokenAmount: wei('1'),
+      slippage: 0.5,
+    })
+    await factory.executeTx()
+  })
+
+  test('can redeem with ETH', async () => {
+    await factory.fetchQuote({
+      isMinting: false,
+      inputToken: btc2x,
+      outputToken: eth,
+      indexTokenAmount: wei('1'),
+      slippage: 0.5,
+    })
+    await factory.executeTx()
+  })
+})
