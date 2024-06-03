5cc45f3e9f40d00eab4233cd0447ad80803211c1
JD <jann.driessen@gmx.de>
2024-06-03 15:17:54 +0200

    feat: add flash mint hyeth contract address

--- src/constants/contracts.ts
+++ src/constants/contracts.ts
@@ -1,4 +1,7 @@
 // Index Protocol
+export const FlashMintHyEthAddress =
+  '0x1C19C605CB071e22a0a46bC9e24B5ce64a28D7Ad'
+
 export const FlashMintLeveragedAddress =
   '0x45c00508C14601fd1C1e296eB3C0e3eEEdCa45D0'
 
