41af54301be88be867b8c5d60ac2a72fcad92737
JD <jann.driessen@gmx.de>
2024-06-04 15:01:25 +0200

    test: update test for redemption quotes

--- src/quote/flashmint/hyeth/provider.test.ts
+++ src/quote/flashmint/hyeth/provider.test.ts
@@ -153,7 +153,6 @@ describe('FlashMintHyEthQuoteProvider()', () => {
     )
   })
 
-  //   test('returns a quote for minting w/ ERC20', async () => {
   test('returns a quote for redeeming to ETH', async () => {
     const request = {
       isMinting: false,
@@ -185,5 +184,50 @@ describe('FlashMintHyEthQuoteProvider()', () => {
       },
     ]
     expect(quote.componentsSwapData).toStrictEqual(componentSwapDataRedeem)
+    expect(quote.swapDataEthToInputOutputToken).toBeNull()
+    expect(quote.swapDataInputTokenToEth).toBeNull()
+  })
+
+  test('returns a quote for redeeming to ERC-20', async () => {
+    const request = {
+      isMinting: false,
+      inputToken: {
+        symbol: indexToken.symbol,
+        decimals: 18,
+        address: indexToken.address!,
+      },
+      outputToken: usdc,
+      indexTokenAmount: wei(1).toBigInt(),
+      slippage: 0.5,
+    }
+    const quoteProvider = new FlashMintHyEthQuoteProvider(rpcUrl)
+    const quote = await quoteProvider.getQuote(request)
+    if (!quote) fail()
+    expect(quote.indexTokenAmount).toEqual(request.indexTokenAmount)
+    expect(quote.inputOutputTokenAmount > 0).toBe(true)
+    const componentSwapDataRedeem = [
+      noopSwapData,
+      noopSwapData,
+      noopSwapData,
+      noopSwapData,
+      noopSwapData,
+      {
+        exchange: Exchange.UniV3,
+        fees: [500],
+        path: [USDC.address, WETH.address],
+        pool: AddressZero,
+      },
+    ]
+    expect(quote.componentsSwapData).toStrictEqual(componentSwapDataRedeem)
+    expect(quote.swapDataInputTokenToEth).toBeNull()
+    const swapDataEthToInputToken = {
+      path: [WETH.address, USDC.address],
+      fees: [500],
+      pool: AddressZero,
+      exchange: Exchange.UniV3,
+    }
+    expect(quote.swapDataEthToInputOutputToken).toStrictEqual(
+      swapDataEthToInputToken
+    )
   })
 })
