79418bff35b1f54a62e1a6f46b02dd9f0ee3f31a
JD <jann.driessen@gmx.de>
2024-06-19 11:33:10 +0200

    feat: add basic quote fetching

---
+++ src/quote/swap/adapters/lifi/index.test.ts
@@ -0,0 +1,42 @@
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
+import 'dotenv/config'
+
+import { EthAddress } from 'constants/addresses'
+
+import { LiFiSwapQuoteProvider } from './'
+
+const apiKey = process.env.LIFI_API_KEY!
+
+const ETH = EthAddress
+const USDC = '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48'
+const ONE = '1000000000000000000'
+
+describe('LifiSwapQuoteProvider', () => {
+  test('getting a swap quote for a specified output amount', async () => {
+    const request = {
+      chainId: 1,
+      inputToken: ETH,
+      outputToken: USDC,
+      outputAmount: '1000000',
+    }
+    const provider = new LiFiSwapQuoteProvider(apiKey)
+    const quote = await provider.getSwapQuote(request)
+    if (!quote) fail()
+    expect(quote).not.toBeNull()
+    expect(quote.inputAmount).not.toBeNull()
+  })
+
+  test('getting a swap quote for a specified input amount', async () => {
+    const request = {
+      chainId: 1,
+      inputToken: ETH,
+      outputToken: USDC,
+      inputAmount: ONE,
+    }
+    const provider = new LiFiSwapQuoteProvider(apiKey)
+    const quote = await provider.getSwapQuote(request)
+    if (!quote) fail()
+    expect(quote).not.toBeNull()
+    expect(quote.inputAmount).not.toBeNull()
+  })
+})
--- src/quote/swap/adapters/lifi/index.ts
+++ src/quote/swap/adapters/lifi/index.ts
@@ -1,4 +1,11 @@
 import {
+  ContractCallsQuoteRequest,
+  getContractCallsQuote,
+  getQuote,
+  LiFiStep,
+} from '@lifi/sdk'
+
+import {
   SwapQuote,
   SwapQuoteProvider,
   SwapQuoteRequest,
@@ -8,7 +15,70 @@ export class LiFiSwapQuoteProvider implements SwapQuoteProvider {
   constructor(readonly apiKey: string) {}
 
   async getSwapQuote(request: SwapQuoteRequest): Promise<SwapQuote | null> {
-    // TODO: based on input/output amount set, take different route
-    return null
+    // TODO:
+    const address = '0xd8dA6BF26964aF9D7eEd9e03E53415D37aA96045'
+    const {
+      chainId,
+      inputAmount,
+      inputToken,
+      outputAmount,
+      outputToken,
+      slippage,
+    } = request
+    if (!inputAmount && !outputAmount) {
+      throw new Error('Error - either input or output amount must be set')
+    }
+    try {
+      let result: LiFiStep | null = null
+      if (outputAmount) {
+        // https://github.com/lifinance/sdk/blob/main/src/services/api.ts#L140
+        const quoteRequest: ContractCallsQuoteRequest = {
+          contractCalls: [],
+          fromAddress: address,
+          fromChain: chainId,
+          fromToken: inputToken,
+          toAmount: outputAmount ?? '',
+          toChain: chainId,
+          toToken: outputToken,
+        }
+        result = await getContractCallsQuote(quoteRequest, undefined)
+        console.log('LI.FI')
+        console.log('////////')
+        console.log(result.estimate)
+      } else {
+        // https://github.com/lifinance/sdk/blob/main/src/services/api.ts#L68
+        const quoteRequest = {
+          fromAddress: address,
+          fromChain: chainId,
+          fromToken: inputToken,
+          fromAmount: inputAmount ?? '',
+          toChain: chainId,
+          toToken: outputToken,
+        }
+        result = await getQuote(quoteRequest)
+      }
+
+      if (!result) {
+        throw new Error('no estimate')
+      }
+      const estimate = result.estimate
+
+      console.log(estimate)
+      return {
+        chainId,
+        inputToken,
+        outputToken,
+        inputAmount: estimate.fromAmount,
+        outputAmount: estimate.toAmount,
+        callData: result.transactionRequest?.data ?? '0x',
+        slippage: slippage ?? 0,
+        // TODO:
+        swapData: null,
+      }
+    } catch (error) {
+      console.log('Error getting LiFi swap quote:')
+      console.log(error)
+      return null
+    }
   }
 }
