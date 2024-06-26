bfdd145e8f8b751d638e66672e8bba910859b631
JD <jann.driessen@gmx.de>
2024-06-26 15:35:32 +0200

    lint: add lifi step type

--- src/quote/swap/adapters/lifi/swap-data.ts
+++ src/quote/swap/adapters/lifi/swap-data.ts
@@ -1,15 +1,18 @@
+import { LiFiStep } from '@lifi/sdk'
+
 import { AddressZero } from 'constants/addresses'
 import { Exchange, SwapData } from 'utils'
 
-export function getSwapData(lifiResult: any): SwapData | null {
+export function getSwapData(lifiResult: LiFiStep): SwapData | null {
+  const feeCosts = lifiResult.estimate.feeCosts ?? null
   const steps = lifiResult.includedSteps
   const step = steps[0]
-  if (!step) return null
+  if (!step || !feeCosts) return null
   const isUniswap = lifiResult.toolDetails.name
     .toLowerCase()
     .includes('uniswap')
   const exchange = isUniswap ? Exchange.UniV3 : Exchange.Sushiswap
-  const fees = lifiResult.estimate.feeCosts[0].percentage * 100000
+  const fees = Number(feeCosts[0].percentage) * 100000
   return {
     exchange,
     path: [
