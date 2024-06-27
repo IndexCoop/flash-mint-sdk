1b3cfb43e6b2fcb878b1a9e5ba01b541318a0bc7
JD <jann.driessen@gmx.de>
2024-06-27 10:12:14 +0200

    feat: update to flash mint hyeth v2 address

--- src/constants/contracts.ts
+++ src/constants/contracts.ts
@@ -1,6 +1,6 @@
 // Index Protocol
 export const FlashMintHyEthAddress =
-  '0xC290c371F5a36970AfF00bbffDBFADd81820109a'
+  '0x940ECB16416fE52856e8653B2958Bfd556aA6A7E'
 
 export const FlashMintLeveragedAddress =
   '0x45c00508C14601fd1C1e296eB3C0e3eEEdCa45D0'
