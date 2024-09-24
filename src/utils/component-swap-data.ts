606e319e1c31ab7cbff49598983a8a94ba356596
JD <jann.driessen@gmx.de>
2024-09-24 16:20:25 +0100

    feat: add amounts for erc4262

--- src/utils/component-swap-data.ts
+++ src/utils/component-swap-data.ts
@@ -49,90 +49,6 @@ interface WrappedToken {
   }
 }
 
-// const erc4626Abi = [
-//   'constructor(address _morpho, address _morphoToken, address _lens, address _recipient)',
-//   'error ZeroAddress()',
-//   'event Accrued(address indexed user, uint256 index, uint256 unclaimed)',
-//   'event Approval(address indexed owner, address indexed spender, uint256 value)',
-//   'event Claimed(address indexed user, uint256 claimed)',
-//   'event Deposit(address indexed caller, address indexed owner, uint256 assets, uint256 shares)',
-//   'event Initialized(uint8 version)',
-//   'event OwnershipTransferred(address indexed previousOwner, address indexed newOwner)',
-//   'event RewardsTransferred(address recipient, uint256 amount)',
-//   'event Transfer(address indexed from, address indexed to, uint256 value)',
-//   'event Withdraw(address indexed caller, address indexed receiver, address indexed owner, uint256 assets, uint256 shares)',
-//   'function allowance(address owner, address spender) view returns (uint256)',
-//   'function approve(address spender, uint256 amount) returns (bool)',
-//   'function asset() view returns (address)',
-//   'function balanceOf(address account) view returns (uint256)',
-//   'function claimRewards(address _user) returns (uint256 rewardsAmount)',
-//   'function comp() view returns (address)',
-//   'function convertToAssets(uint256 shares) view returns (uint256 assets)',
-//   'function convertToShares(uint256 assets) view returns (uint256 shares)',
-//   'function decimals() view returns (uint8)',
-//   'function decreaseAllowance(address spender, uint256 subtractedValue) returns (bool)',
-//   'function deposit(uint256 assets, address receiver) returns (uint256)',
-//   'function increaseAllowance(address spender, uint256 addedValue) returns (bool)',
-//   'function initialize(address _poolToken, string _name, string _symbol, uint256 _initialDeposit)',
-//   'function lens() view returns (address)',
-//   'function maxDeposit(address) view returns (uint256)',
-//   'function maxMint(address) view returns (uint256)',
-//   'function maxRedeem(address owner) view returns (uint256)',
-//   'function maxWithdraw(address owner) view returns (uint256)',
-//   'function mint(uint256 shares, address receiver) returns (uint256)',
-//   'function morpho() view returns (address)',
-//   'function morphoToken() view returns (address)',
-//   'function name() view returns (string)',
-//   'function owner() view returns (address)',
-//   'function poolToken() view returns (address)',
-//   'function previewDeposit(uint256 assets) view returns (uint256)',
-//   'function previewMint(uint256 shares) view returns (uint256)',
-//   'function previewRedeem(uint256 shares) view returns (uint256)',
-//   'function previewWithdraw(uint256 assets) view returns (uint256)',
-//   'function recipient() view returns (address)',
-//   'function redeem(uint256 shares, address receiver, address owner) returns (uint256)',
-//   'function renounceOwnership()',
-//   'function rewardsIndex() view returns (uint256)',
-//   'function symbol() view returns (string)',
-//   'function totalAssets() view returns (uint256)',
-//   'function totalSupply() view returns (uint256)',
-//   'function transfer(address to, uint256 amount) returns (bool)',
-//   'function transferFrom(address from, address to, uint256 amount) returns (bool)',
-//   'function transferOwnership(address newOwner)',
-//   'function transferRewards()',
-//   'function userRewards(address) view returns (uint128 index, uint128 unclaimed)',
-//   'function wEth() view returns (address)',
-//   'function withdraw(uint256 assets, address receiver, address owner) returns (uint256)',
-// ]
-
-// const isFCASH = (address: string) =>
-//   [
-//     '0x278039398A5eb29b6c2FB43789a38A84C6085266',
-//     '0xe09B1968851478f20a43959d8a212051367dF01A',
-//   ].includes(address)
-
-// const getAmountOfAssetToObtainShares = async () =>
-//   component: string,
-//   shares: BigNumber,
-//   provider: JsonRpcProvider
-//   slippage = DEFAULT_SLIPPAGE // 1 = 1%
-//   {
-//   const componentContract = new Contract(component, erc4626Abi, provider)
-//   // Convert slippage to a BigNumber, do rounding to avoid weird JS precision loss
-//   const defaultSlippageBN = BigNumber.from(Math.round(slippage * 10000))
-//   // if FCASH, increase slippage x3
-//   const slippageBigNumber = isFCASH(component)
-//     ? defaultSlippageBN.mul(3)
-//     : defaultSlippageBN
-
-//   // Calculate the multiplier (1 + slippage)
-//   const multiplier = BigNumber.from(10000).add(slippageBigNumber)
-
-//   const buyUnderlyingAmount: BigNumber =
-//     await componentContract.convertToAssets(shares)
-//   return buyUnderlyingAmount.mul(multiplier).div(10000)
-//   }
-
 interface IssuanceRequest extends ComponentSwapDataRequest {
   inputToken: string
 }
@@ -150,32 +66,41 @@ export async function getIssuanceComponentSwapData(
     inputToken,
   } = request
   const issuance = getIssuanceContract(indexTokenSymbol, rpcUrl)
-  const [issuanceComponents] = await issuance.getRequiredComponentIssuanceUnits(
-    indexToken,
-    indexTokenAmount
-  )
+  const [issuanceComponents, issuanceUnits] =
+    await issuance.getRequiredComponentIssuanceUnits(
+      indexToken,
+      indexTokenAmount
+    )
   const underlyingERC20sPromises: Promise<WrappedToken>[] =
     issuanceComponents.map((component: string) =>
       getUnderlyingErc20(component, chainId)
     )
+  const amountPromises = issuanceComponents.map(
+    (component: Address, index: number) =>
+      getAmount(component, issuanceUnits[index], chainId)
+  )
   const wrappedTokens = await Promise.all(underlyingERC20sPromises)
-  // TODO:
-  // const buyAmountsPromises = issuanceComponents.map(
-  //   (component: string, index: number) =>
-  //     getAmountOfAssetToObtainShares(component, issuanceUnits[index], provider)
-  // )
-  //   const buyAmounts = await Promise.all(buyAmountsPromises)
-  const buyAmounts = issuanceComponents.map(() => BigNumber.from(0))
+  const amounts = await Promise.all(amountPromises)
   const swapPromises: Promise<SwapQuote | null>[] = issuanceComponents.map(
     (_: string, index: number) => {
       const wrappedToken = wrappedTokens[index]
       const underlyingERC20 = wrappedToken.underlyingErc20
+      console.log(
+        underlyingERC20.symbol === USDC.symbol,
+        underlyingERC20.symbol,
+        USDC.symbol
+      )
+      console.log(
+        isSameAddress(underlyingERC20.address, inputToken),
+        underlyingERC20.address,
+        inputToken
+      )
       if (isSameAddress(underlyingERC20.address, inputToken)) return null
       return swapQuoteProvider.getSwapQuote({
         chainId,
         inputToken,
         outputToken: underlyingERC20.address,
-        outputAmount: buyAmounts[index].toString(),
+        outputAmount: amounts[index].toString(),
         // TODO: needed for USDCY?
         //   includedSources: 'Uniswap_V3',
       })
@@ -185,7 +110,7 @@ export async function getIssuanceComponentSwapData(
   return buildComponentSwapData(
     issuanceComponents,
     wrappedTokens,
-    buyAmounts,
+    amounts,
     swapData
   )
 }
@@ -207,7 +132,7 @@ export async function getRedemptionComponentSwapData(
     outputToken,
   } = request
   const issuance = getIssuanceContract(indexTokenSymbol, rpcUrl)
-  const [issuanceComponents] =
+  const [issuanceComponents, issuanceUnits] =
     await issuance.getRequiredComponentRedemptionUnits(
       indexToken,
       indexTokenAmount
@@ -216,19 +141,13 @@ export async function getRedemptionComponentSwapData(
     issuanceComponents.map((component: string) =>
       getUnderlyingErc20(component, chainId)
     )
+  const amountPromises = issuanceComponents.map(
+    (component: Address, index: number) =>
+      getAmount(component, issuanceUnits[index], chainId)
+  )
   const wrappedTokens = await Promise.all(underlyingERC20sPromises)
-  // TODO: check google docs
-  // const buyAmountsPromises = issuanceComponents.map(
-  //   (component: string, index: number) =>
-  //     getAmountOfAssetToObtainShares(
-  //       component,
-  //       issuanceUnits[index],
-  //       provider,
-  //       -DEFAULT_SLIPPAGE
-  //     )
-  // )
-  // const buyAmounts = await Promise.all(buyAmountsPromises)
-  const buyAmounts = issuanceComponents.map(() => BigNumber.from(0))
+  const amounts = await Promise.all(amountPromises)
+  console.log(wrappedTokens)
   const swapPromises: Promise<SwapQuote | null>[] = issuanceComponents.map(
     (_: string, index: number) => {
       const wrappedToken = wrappedTokens[index]
@@ -237,7 +156,7 @@ export async function getRedemptionComponentSwapData(
       return swapQuoteProvider.getSwapQuote({
         chainId,
         inputToken: underlyingERC20.address,
-        inputAmount: buyAmounts[index].toString(),
+        inputAmount: amounts[index].toString(),
         outputToken,
         // TODO: needed for USDCY?
         //   includedSources: 'Uniswap_V3',
@@ -248,7 +167,7 @@ export async function getRedemptionComponentSwapData(
   return buildComponentSwapData(
     issuanceComponents,
     wrappedTokens,
-    buyAmounts,
+    amounts,
     swapData
   )
 }
@@ -269,6 +188,58 @@ function buildComponentSwapData(
       dexData,
     }
   })
+}
+
+async function getAmount(
+  component: Address,
+  issuanceUnits: bigint,
+  chainId: number
+): Promise<bigint> {
+  try {
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    const publicClient = createClient(chainId)!
+    const erc4626Abi = [
+      'function convertToAssets(uint256 shares) view returns (uint256 assets)',
+      'function previewDeposit(uint256 assets) view returns (uint256)',
+      'function previewMint(uint256 shares) view returns (uint256)',
+      'function previewRedeem(uint256 shares) view returns (uint256)',
+      'function previewWithdraw(uint256 assets) view returns (uint256)',
+    ]
+    const assets: bigint = (await publicClient.readContract({
+      address: component as Address,
+      abi: erc4626Abi,
+      functionName: 'convertToAssets',
+      args: [issuanceUnits],
+    })) as bigint
+    return assets
+  } catch {
+    // TODO: apply slippage to issuance units amount (for all none erc4262)
+    return BigInt(0)
+  }
+  // const isFCASH = (address: string) =>
+  //   [
+  //     '0x278039398A5eb29b6c2FB43789a38A84C6085266',
+  //     '0xe09B1968851478f20a43959d8a212051367dF01A',
+  //   ].includes(address)
+  // const getAmountOfAssetToObtainShares = async () =>
+  //   component: string,
+  //   shares: BigNumber,
+  //   provider: JsonRpcProvider
+  //   slippage = DEFAULT_SLIPPAGE // 1 = 1%
+  //   {
+  //   const componentContract = new Contract(component, erc4626Abi, provider)
+  //   // Convert slippage to a BigNumber, do rounding to avoid weird JS precision loss
+  //   const defaultSlippageBN = BigNumber.from(Math.round(slippage * 10000))
+  //   // if FCASH, increase slippage x3
+  //   const slippageBigNumber = isFCASH(component)
+  //     ? defaultSlippageBN.mul(3)
+  //     : defaultSlippageBN
+  //   // Calculate the multiplier (1 + slippage)
+  //   const multiplier = BigNumber.from(10000).add(slippageBigNumber)
+  //   const buyUnderlyingAmount: BigNumber =
+  //     await componentContract.convertToAssets(shares)
+  //   return buyUnderlyingAmount.mul(multiplier).div(10000)
+  //   }
 }
 
 function getIssuanceContract(
