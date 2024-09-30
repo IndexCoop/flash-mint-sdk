5144a002ab10e0b6db82f960f8660f931386d499
JD <jann.driessen@gmx.de>
2024-09-30 13:17:27 +0100

    update wrap data test

--- src/utils/wrap-data.test.ts
+++ src/utils/wrap-data.test.ts
@@ -8,14 +8,15 @@ describe('getWrapData()', () => {
 
   test.only('returns correct wrap data for icUSD', async () => {
     const wrapData = getWrapData('icUSD')
-    expect(wrapData.length).toBe(5)
-    expect(wrapData[0].integrationName).toBe('')
-    expect(wrapData[1].integrationName).toBe('Aave_V3_Wrap_V2_Adapter')
-    expect(wrapData[2].integrationName).toBe('Compound_V3_USDC_Wrap_V2_Adapter')
-    expect(wrapData[3].integrationName).toBe('Aave_V2_Wrap_V2_Adapter')
-    expect(wrapData[4].integrationName).toBe('ERC4626_Wrap_V2_Adapter')
-    wrapData.forEach((data) => {
-      expect(data.wrapData).toBe('0x0000000000000000000000000000000000000000')
-    })
+    // TODO: update for components (after presale/rebalance)
+    expect(wrapData.length).toBe(1)
+    // expect(wrapData[0].integrationName).toBe('')
+    // expect(wrapData[1].integrationName).toBe('Aave_V3_Wrap_V2_Adapter')
+    // expect(wrapData[2].integrationName).toBe('Compound_V3_USDC_Wrap_V2_Adapter')
+    // expect(wrapData[3].integrationName).toBe('Aave_V2_Wrap_V2_Adapter')
+    // expect(wrapData[4].integrationName).toBe('ERC4626_Wrap_V2_Adapter')
+    // wrapData.forEach((data) => {
+    //   expect(data.wrapData).toBe('0x0000000000000000000000000000000000000000')
+    // })
   })
 })
