54b719aeacae3cbf88abb6fd5739d39388e6187e
JD <jann.driessen@gmx.de>
2024-10-24 19:05:44 +0200

    update tests to test for reserve asset swap data

--- src/quote/flashmint/nav/provider.test.ts
+++ src/quote/flashmint/nav/provider.test.ts
@@ -5,8 +5,9 @@ import {
   QuoteTokens,
 } from 'tests/utils'
 import { wei } from 'utils/numbers'
+import { Exchange, isSameAddress } from 'utils'
+
 import { FlashMintNavQuoteRequest, FlashMintNavQuoteProvider } from './provider'
-import { Exchange } from 'utils'
 
 describe('FlashMintNavQuoteProvider()', () => {
   const { icusd, usdc, weth } = QuoteTokens
@@ -58,8 +59,19 @@ describe('FlashMintNavQuoteProvider()', () => {
     if (!quote) fail()
     expect(quote.inputTokenAmount).toEqual(request.inputTokenAmount)
     expect(quote.outputTokenAmount.gt(0)).toEqual(true)
-    // TODO:
-    expect(quote.reserveAssetSwapData).toBeDefined()
+    // Testing for individual params as changing market conditions could affect
+    // the swap data contents and the test results
+    const { reserveAssetSwapData } = quote
+    expect(reserveAssetSwapData.exchange).not.toBe(Exchange.None)
+    expect(reserveAssetSwapData.fees.length).toBeGreaterThanOrEqual(1)
+    expect(isSameAddress(reserveAssetSwapData.path[0], weth.address)).toBe(true)
+    expect(
+      isSameAddress(
+        reserveAssetSwapData.path[reserveAssetSwapData.path.length - 1],
+        usdc.address
+      )
+    ).toBe(true)
+    expect(reserveAssetSwapData.poolIds).toEqual([])
   })
 
   test('returns a quote redeeming icUSD for USDC', async () => {
@@ -105,7 +117,18 @@ describe('FlashMintNavQuoteProvider()', () => {
     if (!quote) fail()
     expect(quote.inputTokenAmount).toEqual(request.inputTokenAmount)
     expect(quote.outputTokenAmount.gt(0)).toEqual(true)
-    // TODO:
-    expect(quote.reserveAssetSwapData).toBeDefined()
+    // Testing for individual params as changing market conditions could affect
+    // the swap data contents and the test results
+    const { reserveAssetSwapData } = quote
+    expect(reserveAssetSwapData.exchange).not.toBe(Exchange.None)
+    expect(reserveAssetSwapData.fees.length).toBeGreaterThanOrEqual(1)
+    expect(isSameAddress(reserveAssetSwapData.path[0], usdc.address)).toBe(true)
+    expect(
+      isSameAddress(
+        reserveAssetSwapData.path[reserveAssetSwapData.path.length - 1],
+        weth.address
+      )
+    ).toBe(true)
+    expect(reserveAssetSwapData.poolIds).toEqual([])
   })
 })
