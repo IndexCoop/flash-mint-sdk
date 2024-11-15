23b439c511bc2791455f0a8d060f358baff82e19
JD <jann.driessen@gmx.de>
2024-11-15 16:14:23 +0100

    fix wrap data test

--- docs/icusd/README.md
+++ docs/icusd/README.md
@@ -3,4 +3,5 @@
 ## Composition Updates
 
 1. Update wrap data according to the new composition watching integration names [here](src/utils/wrap-data.ts)
-2. Make sure nothing changes about how to generate the (issuance) amounts for the different components in the [component swap data](src/utils/component-swap-data.ts)
+2. Update wrap data tests accordingly
+3. Make sure nothing changes about how to generate the (issuance) amounts for the different components in the [component swap data](src/utils/component-swap-data.ts)
--- src/utils/wrap-data.test.ts
+++ src/utils/wrap-data.test.ts
@@ -8,12 +8,15 @@ describe('getWrapData()', () => {
 
   test.only('returns correct wrap data for icUSD', async () => {
     const wrapData = getWrapData('icUSD')
-    expect(wrapData.length).toBe(5)
+    expect(wrapData.length).toBe(8)
     expect(wrapData[0].integrationName).toBe('AaveV3WrapV2Adapter')
     expect(wrapData[1].integrationName).toBe('CompoundV3WrapV2Adapter')
     expect(wrapData[2].integrationName).toBe('ERC4626WrapV2Adapter')
     expect(wrapData[3].integrationName).toBe('ERC4626WrapV2Adapter')
     expect(wrapData[4].integrationName).toBe('ERC4626WrapV2Adapter')
+    expect(wrapData[5].integrationName).toBe('')
+    expect(wrapData[6].integrationName).toBe('ERC4626WrapV2Adapter')
+    expect(wrapData[7].integrationName).toBe('ERC4626WrapV2Adapter')
     wrapData.forEach((data) => {
       expect(data.wrapData).toBe('0x0000000000000000000000000000000000000000')
     })
