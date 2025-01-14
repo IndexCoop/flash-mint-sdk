cf13909aa0714d1fa9c880cf673f07bf37a89e68
JD <jann.driessen@gmx.de>
2025-01-14 14:25:55 -0500

    add integration tests

---
+++ src/tests/base/btc2x.test.ts
@@ -0,0 +1,42 @@
+import { getTokenByChainAndSymbol } from '@indexcoop/tokenlists'
+import { ChainId } from 'constants/chains'
+import {
+  QuoteTokens,
+  type TestFactory,
+  getTestFactoryZeroEx,
+  wei,
+} from 'tests/utils'
+
+describe('BTC2X (Base)', () => {
+  const chainId = ChainId.Base
+  const { eth } = QuoteTokens
+  const indexToken = getTokenByChainAndSymbol(chainId, 'BTC2X')
+  let factory: TestFactory
+  beforeEach(async () => {
+    factory = getTestFactoryZeroEx(0, chainId)
+  })
+
+  test('can mint with ETH', async () => {
+    await factory.fetchQuote({
+      isMinting: true,
+      inputToken: eth,
+      outputToken: indexToken,
+      indexTokenAmount: wei('1').toString(),
+      inputTokenAmount: wei(0.5).toString(),
+      slippage: 0.5,
+    })
+    await factory.executeTx()
+  })
+
+  test.skip('can redeem with ETH', async () => {
+    await factory.fetchQuote({
+      isMinting: false,
+      inputToken: indexToken,
+      outputToken: eth,
+      indexTokenAmount: wei('1').toString(),
+      inputTokenAmount: wei('1').toString(),
+      slippage: 0.5,
+    })
+    await factory.executeTx()
+  })
+})
