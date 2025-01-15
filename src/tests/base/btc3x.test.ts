5a56374a39579a4443c36340ca72ebceb3dc7351
JD <jann.driessen@gmx.de>
2025-01-14 20:32:10 -0500

    add tests for usdc. update slippage for redeem to eth

--- src/tests/base/btc3x.test.ts
+++ src/tests/base/btc3x.test.ts
@@ -4,6 +4,7 @@ import {
   QuoteTokens,
   type TestFactory,
   getTestFactoryZeroEx,
+  transferFromWhale,
   wei,
 } from 'tests/utils'
 
@@ -11,6 +12,7 @@ describe('BTC3X (Base)', () => {
   const chainId = ChainId.Base
   const { eth } = QuoteTokens
   const indexToken = getTokenByChainAndSymbol(chainId, 'BTC3X')
+  const usdc = getTokenByChainAndSymbol(chainId, 'USDC')
   let factory: TestFactory
   beforeEach(async () => {
     factory = getTestFactoryZeroEx(0, chainId)
@@ -28,14 +30,46 @@ describe('BTC3X (Base)', () => {
     await factory.executeTx()
   })
 
-  test.skip('can redeem with ETH', async () => {
+  test('can mint with USDC', async () => {
+    const quote = await factory.fetchQuote({
+      isMinting: true,
+      inputToken: usdc,
+      outputToken: indexToken,
+      indexTokenAmount: wei('1').toString(),
+      inputTokenAmount: wei('1018', usdc.decimals).toString(),
+      slippage: 0.5,
+    })
+    const whale = '0x8dB0f952B8B6A462445C732C41Ec2937bCae9c35'
+    await transferFromWhale(
+      whale,
+      factory.getSigner().address,
+      wei('10000', quote.inputToken.decimals),
+      quote.inputToken.address,
+      factory.getProvider(),
+    )
+    await factory.executeTx()
+  })
+
+  test('can redeem to ETH', async () => {
     await factory.fetchQuote({
       isMinting: false,
       inputToken: indexToken,
       outputToken: eth,
       indexTokenAmount: wei('1').toString(),
       inputTokenAmount: wei('1').toString(),
-      slippage: 0.5,
+      slippage: 0.6,
+    })
+    await factory.executeTx()
+  })
+
+  test('can redeem to USDC', async () => {
+    await factory.fetchQuote({
+      isMinting: false,
+      inputToken: indexToken,
+      outputToken: usdc,
+      indexTokenAmount: wei('1').toString(),
+      inputTokenAmount: wei('1').toString(),
+      slippage: 0.6,
     })
     await factory.executeTx()
   })
