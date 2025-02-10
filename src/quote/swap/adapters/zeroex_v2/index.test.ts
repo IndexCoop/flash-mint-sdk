de42d1a0d7a98674900f39617aaf08aad8aaaefa
JD <jann.driessen@gmx.de>
2025-02-10 12:06:16 +0100

    add error handling for insufficient liquidity

--- src/quote/swap/adapters/zeroex_v2/index.test.ts
+++ src/quote/swap/adapters/zeroex_v2/index.test.ts
@@ -19,6 +19,24 @@ describe('ZeroExApi', () => {
       process.env.ZEROEX_API_KEY!,
     )
     const quote = await quoteProvider.getSwapQuote(request)
+    // TODO:
     console.log(quote, 'test')
+  })
+
+  test('should handle no liquidity available', async () => {
+    const request = {
+      address: '0xd8dA6BF26964aF9D7eEd9e03E53415D37aA96045',
+      chainId: 1,
+      inputToken: ETH,
+      outputToken: '0x1B5E16C5b20Fb5EE87C61fE9Afe735Cca3B21A65', // ic21
+      inputAmount: '1000000000000000000',
+      slippage: 0.5,
+    }
+    const quoteProvider = new ZeroExV2SwapQuoteProvider(
+      process.env.ZEROEX_API_KEY!,
+    )
+    await expect(quoteProvider.getSwapQuote(request)).rejects.toThrow(
+      'Insufficient liquidity for swap',
+    )
   })
 })
--- src/quote/swap/adapters/zeroex_v2/index.ts
+++ src/quote/swap/adapters/zeroex_v2/index.ts
@@ -4,19 +4,24 @@ import type {
   SwapQuoteRequest,
 } from 'quote/swap/interfaces'
 import { getClientV2 } from './client'
+import {
+  ZeroExV2SwapQuoteProviderError,
+  ZeroExV2SwapQuoteProviderErrorType,
+} from './errors'
 import { convertTo0xSlippage } from './utils'
 
 export class ZeroExV2SwapQuoteProvider implements SwapQuoteProvider {
   constructor(readonly apiKey: string) {}
 
   async getSwapQuote(request: SwapQuoteRequest): Promise<SwapQuote | null> {
-    try {
-      const path = this.getPath(request)
-      const res = await getClientV2(path, this.apiKey)
-      console.log(res)
-      // const res: ZeroExApiSwapResponse = response.data
-    } catch (err: unknown) {
-      console.error(err)
+    const path = this.getPath(request)
+    const swapResponse = await getClientV2(path, this.apiKey)
+    console.log(swapResponse)
+    if (!swapResponse.liquidityAvailable) {
+      throw new ZeroExV2SwapQuoteProviderError(
+        ZeroExV2SwapQuoteProviderErrorType.insufficientLiquidity,
+        'Insufficient liquidity for swap',
+      )
     }
     return null
   }
