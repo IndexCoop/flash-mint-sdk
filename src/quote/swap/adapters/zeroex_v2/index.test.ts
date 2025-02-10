c413206af2a167251bad25fad061dd6123d51335
JD <jann.driessen@gmx.de>
2025-02-10 12:12:00 +0100

    use one provider var in tests

--- src/quote/swap/adapters/zeroex_v2/index.test.ts
+++ src/quote/swap/adapters/zeroex_v2/index.test.ts
@@ -6,6 +6,12 @@ const ETH = '0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE'
 const USDC = '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48'
 
 describe('ZeroExApi', () => {
+  let quoteProvider: ZeroExV2SwapQuoteProvider
+
+  beforeEach(() => {
+    quoteProvider = new ZeroExV2SwapQuoteProvider(process.env.ZEROEX_API_KEY!)
+  })
+
   test('building a url', async () => {
     const request = {
       address: '0xd8dA6BF26964aF9D7eEd9e03E53415D37aA96045',
@@ -15,9 +21,6 @@ describe('ZeroExApi', () => {
       inputAmount: '100000000',
       slippage: 0.1,
     }
-    const quoteProvider = new ZeroExV2SwapQuoteProvider(
-      process.env.ZEROEX_API_KEY!,
-    )
     const quote = await quoteProvider.getSwapQuote(request)
     // TODO:
     console.log(quote, 'test')
@@ -32,9 +35,6 @@ describe('ZeroExApi', () => {
       inputAmount: '1000000000000000000',
       slippage: 0.5,
     }
-    const quoteProvider = new ZeroExV2SwapQuoteProvider(
-      process.env.ZEROEX_API_KEY!,
-    )
     await expect(quoteProvider.getSwapQuote(request)).rejects.toThrow(
       'Insufficient liquidity for swap',
     )
