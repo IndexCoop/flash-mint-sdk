2fff715f27b93ff8feacc19cb0e0c4347a473fb3
JD <jann.driessen@gmx.de>
2025-02-10 11:59:06 +0100

    add response interfaces

--- src/quote/swap/adapters/zeroex_v2/client.ts
+++ src/quote/swap/adapters/zeroex_v2/client.ts
@@ -1,5 +1,28 @@
 import axios from 'axios'
 
+interface ZeroExApiV2SwapResponse {
+  blockNumber: string
+  buyAmount: string
+  buyToken: string
+  // fees
+  gas: string | null
+  gasPrice: string
+  // issues
+  liquidityAvailable: boolean
+  minBuyAmount: string
+  // route
+  sellAmount: string
+  sellToken: string
+  // tokenMetadata
+  totalNetworkFee: string | null
+  zid: string
+}
+
+interface ZeroExApiV2SwapResponseNoLiquidity {
+  liquidityAvailable: boolean
+  zid: string
+}
+
 export async function getClientV2(path: string, apiKey: string) {
   const config = {
     headers: {
@@ -9,5 +32,7 @@ export async function getClientV2(path: string, apiKey: string) {
   }
   const url = `https://api.0x.org/swap/allowance-holder/quote?${path}`
   const res = await axios.get(url, config)
-  return res.data
+  return res.data as
+    | ZeroExApiV2SwapResponse
+    | ZeroExApiV2SwapResponseNoLiquidity
 }
