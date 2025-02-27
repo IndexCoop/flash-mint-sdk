bdc56ae9eab0555f91c8a05a5f58fd01f3c60ec9
JD <jann.driessen@gmx.de>
2025-02-27 09:34:30 +0100

    add static swap data for usui

--- src/quote/flashmint/leveraged-morpho/provider.test.ts
+++ src/quote/flashmint/leveraged-morpho/provider.test.ts
@@ -16,50 +16,22 @@ const btc2x = getTokenByChainAndSymbol(chainId, 'BTC2X')
 const cbBTC = getTokenByChainAndSymbol(chainId, 'cbBTC')
 const eth = ETH
 const usdc = getTokenByChainAndSymbol(chainId, 'USDC')
+const uSui = getTokenByChainAndSymbol(chainId, 'uSUI')
+const uSui2x = getTokenByChainAndSymbol(chainId, 'uSUI2x')
+const uSui3x = getTokenByChainAndSymbol(chainId, 'uSUI3x')
 const weth = getTokenByChainAndSymbol(chainId, 'WETH')
+const wstEth15x = getTokenByChainAndSymbol(chainId, 'wstETH15x')
 
 const taker = '0xd8dA6BF26964aF9D7eEd9e03E53415D37aA96045'
 
-// TODO: add according tests
-describe.skip('LeveragedMorphoQuoteProvider()', () => {
-  test('returns quote for minting BTC2X - cbBTC', async () => {
-    const request = {
-      chainId,
-      isMinting: true,
-      inputToken: cbBTC,
-      outputToken: btc2x,
-      inputAmount: wei(0.015, 8).toString(),
-      outputAmount: wei(1).toString(),
-      slippage: 0.5,
-      taker,
-    }
-    const quoteProvider = new LeveragedMorphoQuoteProvider(
-      rpcUrl,
-      swapQuoteProvider,
-    )
-    const quote = await quoteProvider.getQuote(request)
-    if (!quote) fail()
-    expect(quote.inputAmount.gt(0)).toBe(true)
-    expect(quote.outputAmount.toString()).toEqual(request.outputAmount)
-    // Testing for individual params as changing quotes could affect the results
-    const { swapDataDebtCollateral, swapDataInputOutputToken } = quote
-    expect(swapDataDebtCollateral.exchange).toEqual(
-      Exchange.AerodromeSlipstream,
-    )
-    expect(swapDataDebtCollateral.tickSpacing).toEqual([100])
-    expect(swapDataDebtCollateral.path).toEqual([usdc.address, cbBTC.address])
-    expect(swapDataInputOutputToken.exchange).toEqual(Exchange.None)
-    expect(swapDataInputOutputToken.path).toEqual([])
-    expect(swapDataInputOutputToken.tickSpacing).toEqual([])
-  })
-
-  test('returns quote for minting BTC2X - ETH', async () => {
+describe('LeveragedMorphoQuoteProvider()', () => {
+  test('returns quote for minting uSUI2x - ETH', async () => {
     const request = {
       chainId,
       isMinting: true,
       inputToken: eth,
-      outputToken: btc2x,
-      inputAmount: wei(0.4).toString(),
+      outputToken: uSui2x,
+      inputAmount: wei(0.5).toString(),
       outputAmount: wei(1).toString(),
       slippage: 0.5,
       taker,
@@ -77,22 +49,26 @@ describe.skip('LeveragedMorphoQuoteProvider()', () => {
     expect(swapDataDebtCollateral.exchange).toEqual(
       Exchange.AerodromeSlipstream,
     )
-    expect(swapDataDebtCollateral.tickSpacing).toEqual([100])
-    expect(swapDataDebtCollateral.path).toEqual([usdc.address, cbBTC.address])
+    expect(swapDataDebtCollateral.tickSpacing).toEqual([100, 200])
+    expect(swapDataDebtCollateral.path).toEqual([
+      usdc.address,
+      weth.address,
+      uSui.address,
+    ])
     expect(swapDataInputOutputToken.exchange).toEqual(
       Exchange.AerodromeSlipstream,
     )
-    expect(swapDataInputOutputToken.tickSpacing).toEqual([100])
-    expect(swapDataInputOutputToken.path).toEqual([weth.address, cbBTC.address])
+    expect(swapDataInputOutputToken.path).toEqual([weth.address, uSui.address])
+    expect(swapDataInputOutputToken.tickSpacing).toEqual([200])
   })
 
-  test('returns quote for minting BTC2X - USDC (ERC20)', async () => {
+  test('returns quote for minting uSUI2x - USDC (ERC20)', async () => {
     const indexTokenAmount = wei(1).toString()
     const request = {
       chainId,
       isMinting: true,
       inputToken: usdc,
-      outputToken: btc2x,
+      outputToken: uSui2x,
       inputAmount: wei(0.5).toString(),
       outputAmount: indexTokenAmount,
       slippage: 0.5,
@@ -111,53 +87,29 @@ describe.skip('LeveragedMorphoQuoteProvider()', () => {
     expect(swapDataDebtCollateral.exchange).toEqual(
       Exchange.AerodromeSlipstream,
     )
-    expect(swapDataDebtCollateral.tickSpacing).toEqual([100])
-    expect(swapDataDebtCollateral.path).toEqual([usdc.address, cbBTC.address])
+    expect(swapDataDebtCollateral.tickSpacing).toEqual([100, 200])
+    expect(swapDataDebtCollateral.path).toEqual([
+      usdc.address,
+      weth.address,
+      uSui.address,
+    ])
     expect(swapDataInputOutputToken.exchange).toEqual(
       Exchange.AerodromeSlipstream,
     )
-    expect(swapDataInputOutputToken.tickSpacing).toEqual([100])
-    expect(swapDataInputOutputToken.path).toEqual([usdc.address, cbBTC.address])
+    expect(swapDataInputOutputToken.path).toEqual([
+      usdc.address,
+      weth.address,
+      uSui.address,
+    ])
+    expect(swapDataInputOutputToken.tickSpacing).toEqual([100, 200])
   })
 
-  test('returns quote for redeeming BTC2X - cbBTC', async () => {
+  test('returns quote for redeeming uSUI2x - ETH', async () => {
     const indexTokenAmount = wei(1).toString()
     const request = {
       chainId,
       isMinting: false,
-      inputToken: getTokenByChainAndSymbol(chainId, 'BTC2X'),
-      outputToken: cbBTC,
-      inputAmount: indexTokenAmount,
-      outputAmount: wei(0.5).toString(), // not used for redeeming
-      slippage: 0.5,
-      taker,
-    }
-    const quoteProvider = new LeveragedMorphoQuoteProvider(
-      rpcUrl,
-      swapQuoteProvider,
-    )
-    const quote = await quoteProvider.getQuote(request)
-    if (!quote) fail()
-    const { swapDataDebtCollateral, swapDataInputOutputToken } = quote
-    expect(quote.inputAmount.toString()).toEqual(indexTokenAmount)
-    expect(quote.outputAmount.gt(0)).toBe(true)
-    // Testing for individual params as changing quotes could affect the results
-    expect(swapDataDebtCollateral.exchange).toEqual(
-      Exchange.AerodromeSlipstream,
-    )
-    expect(swapDataDebtCollateral.tickSpacing).toEqual([100])
-    expect(swapDataDebtCollateral.path).toEqual([cbBTC.address, usdc.address])
-    expect(swapDataInputOutputToken.exchange).toEqual(Exchange.None)
-    expect(swapDataInputOutputToken.tickSpacing).toEqual([])
-    expect(swapDataInputOutputToken.path).toEqual([])
-  })
-
-  test('returns quote for redeeming BTC2X - ETH', async () => {
-    const indexTokenAmount = wei(1).toString()
-    const request = {
-      chainId,
-      isMinting: false,
-      inputToken: getTokenByChainAndSymbol(chainId, 'BTC2X'),
+      inputToken: uSui2x,
       outputToken: weth,
       inputAmount: indexTokenAmount,
       outputAmount: wei(0.5).toString(), // not used for redeeming
@@ -177,21 +129,25 @@ describe.skip('LeveragedMorphoQuoteProvider()', () => {
     expect(swapDataDebtCollateral.exchange).toEqual(
       Exchange.AerodromeSlipstream,
     )
-    expect(swapDataDebtCollateral.tickSpacing).toEqual([100])
-    expect(swapDataDebtCollateral.path).toEqual([cbBTC.address, usdc.address])
+    expect(swapDataDebtCollateral.tickSpacing).toEqual([200, 100])
+    expect(swapDataDebtCollateral.path).toEqual([
+      uSui.address,
+      weth.address,
+      usdc.address,
+    ])
     expect(swapDataInputOutputToken.exchange).toEqual(
       Exchange.AerodromeSlipstream,
     )
-    expect(swapDataInputOutputToken.tickSpacing).toEqual([100])
-    expect(swapDataInputOutputToken.path).toEqual([cbBTC.address, weth.address])
+    expect(swapDataInputOutputToken.tickSpacing).toEqual([200])
+    expect(swapDataInputOutputToken.path).toEqual([uSui.address, weth.address])
   })
 
-  test('returns quote for redeeming BTC2X - USDC (ERC20)', async () => {
+  test('returns quote for redeeming uSUI2x - USDC (ERC20)', async () => {
     const indexTokenAmount = wei(1).toString()
     const request = {
       chainId,
       isMinting: false,
-      inputToken: getTokenByChainAndSymbol(chainId, 'BTC2X'),
+      inputToken: uSui2x,
       outputToken: usdc,
       inputAmount: indexTokenAmount,
       outputAmount: wei(0.5).toString(), // not used for redeeming
@@ -211,12 +167,20 @@ describe.skip('LeveragedMorphoQuoteProvider()', () => {
     expect(swapDataDebtCollateral.exchange).toEqual(
       Exchange.AerodromeSlipstream,
     )
-    expect(swapDataDebtCollateral.tickSpacing).toEqual([100])
-    expect(swapDataDebtCollateral.path).toEqual([cbBTC.address, usdc.address])
+    expect(swapDataDebtCollateral.tickSpacing).toEqual([200, 100])
+    expect(swapDataDebtCollateral.path).toEqual([
+      uSui.address,
+      weth.address,
+      usdc.address,
+    ])
     expect(swapDataInputOutputToken.exchange).toEqual(
       Exchange.AerodromeSlipstream,
     )
-    expect(swapDataInputOutputToken.tickSpacing).toEqual([100])
-    expect(swapDataInputOutputToken.path).toEqual([cbBTC.address, usdc.address])
+    expect(swapDataInputOutputToken.tickSpacing).toEqual([200, 100])
+    expect(swapDataInputOutputToken.path).toEqual([
+      uSui.address,
+      weth.address,
+      usdc.address,
+    ])
   })
 })
--- src/quote/swap/adapters/static/index.ts
+++ src/quote/swap/adapters/static/index.ts
@@ -3,7 +3,7 @@ import { base } from 'viem/chains'
 import { AddressZero, EthAddress } from 'constants/addresses'
 import { Exchange } from 'utils'
 
-import { getTokenByChainAndSymbol } from '@indexcoop/tokenlists'
+import { getTokenByChainAndSymbol, isAddressEqual } from '@indexcoop/tokenlists'
 import type {
   SwapQuoteProviderV2,
   SwapQuoteRequestV2,
@@ -11,6 +11,8 @@ import type {
 } from 'quote/swap/interfaces'
 import type { SwapDataV5 } from 'utils'
 import type { Address } from 'viem'
+
+const uSui = getTokenByChainAndSymbol(base.id, 'uSUI')
 
 export class StaticSwapQuoteProvider implements SwapQuoteProviderV2 {
   async getSwapQuote(request: SwapQuoteRequestV2): Promise<SwapQuoteV2 | null> {
@@ -24,13 +26,21 @@ export class StaticSwapQuoteProvider implements SwapQuoteProviderV2 {
       return null
     }
 
-    const swapData: SwapDataV5 = {
+    let swapData: SwapDataV5 = {
       exchange: Exchange.AerodromeSlipstream,
       path: [inputToken, outputToken],
       fees: [],
       pool: AddressZero,
       poolIds: [],
       tickSpacing: [100],
+    }
+
+    const isUSui =
+      isAddressEqual(inputToken, uSui.address) ||
+      isAddressEqual(outputToken, uSui.address)
+
+    if (isUSui) {
+      swapData = this.getUSuiSwapData(inputToken, outputToken)
     }
 
     return {
@@ -49,5 +59,30 @@ export class StaticSwapQuoteProvider implements SwapQuoteProviderV2 {
     return token === EthAddress
       ? getTokenByChainAndSymbol(chainId, 'WETH')!.address
       : (token as Address)
+  }
+
+  getUSuiSwapData(inputToken: Address, outputToken: Address): SwapDataV5 {
+    const weth = getTokenByChainAndSymbol(base.id, 'WETH').address
+    if (isAddressEqual(inputToken, weth) || isAddressEqual(outputToken, weth)) {
+      // WETH/uSUI https://basescan.org/address/0x5C45b0F48c326f79b56709d8F63CE2beE7697106
+      return {
+        exchange: Exchange.AerodromeSlipstream,
+        path: [inputToken, outputToken],
+        fees: [],
+        pool: AddressZero,
+        poolIds: [],
+        tickSpacing: [200],
+      }
+    }
+    // USDC/WETH WETH/uSUI
+    const isRedeeming = isAddressEqual(inputToken, uSui.address)
+    return {
+      exchange: Exchange.AerodromeSlipstream,
+      path: [inputToken, weth, outputToken],
+      fees: [],
+      pool: AddressZero,
+      poolIds: [],
+      tickSpacing: isRedeeming ? [200, 100] : [100, 200],
+    }
   }
 }
