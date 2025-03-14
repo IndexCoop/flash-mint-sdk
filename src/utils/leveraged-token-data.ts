aa983e0017b1e611884c52b2215a0bfe7e78e897
JD <jann.driessen@gmx.de>
2025-03-14 17:41:40 +0100

    add custom get leveraged token data for zero ex

--- src/utils/leveraged-token-data.ts
+++ src/utils/leveraged-token-data.ts
@@ -1,7 +1,10 @@
+import { FlashMintAbis } from 'utils/abis'
+import { createClientWithUrl } from 'utils/clients'
+import { getFlashMintLeveragedContractForToken } from 'utils/contracts'
+
 import type { BigNumber } from '@ethersproject/bignumber'
 import type { JsonRpcProvider } from '@ethersproject/providers'
-
-import { getFlashMintLeveragedContractForToken } from 'utils/contracts'
+import type { Address } from 'viem'
 
 export interface LeveragedTokenData {
   collateralAToken: string
@@ -33,6 +36,36 @@ export async function getLeveragedTokenData(
   } catch (error) {
     // Should this just always fail cause it means there is something wrongly configured?
     console.error('Error getting leveraged token data', error)
+    return null
+  }
+}
+
+type GetLeveragedTokenDataParams = {
+  indexTokenAddress: string
+  indexTokenAmount: bigint
+  isIssuance: boolean
+  isAave: boolean
+}
+
+export async function getLeveragedZeroExTokenData(
+  contract: Address,
+  params: GetLeveragedTokenDataParams,
+  chainId: number,
+  rpcUrl: string,
+): Promise<LeveragedTokenData | null> {
+  try {
+    const { indexTokenAddress, indexTokenAmount, isIssuance, isAave } = params
+    const abi = FlashMintAbis[contract]
+    const publicClient = createClientWithUrl(chainId, rpcUrl)!
+    const data = await publicClient.readContract({
+      address: contract,
+      abi,
+      functionName: 'getLeveragedTokenData',
+      args: [indexTokenAddress, indexTokenAmount, isIssuance, isAave],
+    })
+    return data as LeveragedTokenData | null
+  } catch (error) {
+    console.error('Error getting leveraged token data:', error)
     return null
   }
 }
