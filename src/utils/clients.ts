51b16a36687ac0cfb326d0bb8eaa7aa45621cc5d
JD <jann.driessen@gmx.de>
2025-03-19 12:29:08 +0100

    fix fkn viem types

--- src/utils/clients.ts
+++ src/utils/clients.ts
@@ -3,30 +3,37 @@ import { base, mainnet } from 'viem/chains'
 
 import { ChainId } from 'constants/chains'
 
-export function createClient(chainId: number) {
+import type { PublicClient, Transport } from 'viem'
+
+type Client = PublicClient<Transport, typeof base | typeof mainnet>
+
+export function createClient(chainId: number): Client | null {
   if (chainId === ChainId.Base) {
     return createPublicClient({
       chain: base,
       transport: http(process.env.BASE_ALCHEMY_API),
-    })
+    }) as Client
   }
   if (chainId !== ChainId.Mainnet) return null
   return createPublicClient({
     chain: mainnet,
     transport: http(process.env.MAINNET_ALCHEMY_API),
-  })
+  }) as Client
 }
 
-export function createClientWithUrl(chainId: number, rpcUrl: string) {
+export function createClientWithUrl(
+  chainId: number,
+  rpcUrl: string,
+): Client | null {
   if (chainId === ChainId.Base) {
     return createPublicClient({
       chain: base,
       transport: http(rpcUrl),
-    })
+    }) as Client
   }
   if (chainId !== ChainId.Mainnet) return null
   return createPublicClient({
     chain: mainnet,
     transport: http(rpcUrl),
-  })
+  }) as Client
 }
