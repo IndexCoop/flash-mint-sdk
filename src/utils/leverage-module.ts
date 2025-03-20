d0a37cc2b4933c1c076edb242f3075c37f156293
JD <jann.driessen@gmx.de>
2025-03-20 09:07:00 +0100

    update getting leverage module

--- src/utils/leverage-module.ts
+++ src/utils/leverage-module.ts
@@ -1,8 +1,10 @@
 import { createClientWithUrl } from 'utils/clients'
 import { type Address, parseAbi } from 'viem'
 
-// Base: https://basescan.org/address/0xc06a6e4d9d5ff9d64bd19fc243ad9b6e5a672699
-const AaveV3LeverageModule = '0xC06a6E4d9D5FF9d64BD19fc243aD9B6E5a672699'
+const AaveV3LeverageModule: { [key: number]: Address } = {
+  1: '0x9d08CCeD85A68Bf8A19374ED4B5753aE3Be9F74f',
+  8453: '0xC06a6E4d9D5FF9d64BD19fc243aD9B6E5a672699',
+}
 
 export async function usesAaveLeverageModule(
   indexToken: string,
@@ -15,5 +17,5 @@ export async function usesAaveLeverageModule(
     abi: parseAbi(['function getModules() view returns (address[])']),
     functionName: 'getModules',
   })) as Address[]
-  return modules.includes(AaveV3LeverageModule)
+  return modules.includes(AaveV3LeverageModule[chainId])
 }
