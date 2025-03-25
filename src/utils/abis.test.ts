128b07e90a5a5903db96cc79b44723732e356c45
JD <jann.driessen@gmx.de>
2025-03-25 08:48:38 +0100

    add abi for arbitrum

--- src/utils/abis.test.ts
+++ src/utils/abis.test.ts
@@ -1,8 +1,16 @@
 import FLASHMINT_LEVERAGED_ZEROEX_ABI from 'constants/abis/FlashMintLeveragedZeroEx.json'
+import FLASHMINT_LEVERAGED_ZEROEX_BALANCER_FL_ABI from 'constants/abis/FlashMintLeveragedZeroExBalancerFL.json'
 import { Contracts } from 'constants/contracts'
 import { FlashMintAbis } from 'utils/abis'
+import { arbitrum } from 'viem/chains'
 
 describe('FlashMintAbis', () => {
+  test('returns correct ABI for FlashMintLeveragedZeroEx on Base', async () => {
+    expect(
+      FlashMintAbis[Contracts[arbitrum.id].FlashMintLeveragedZeroEx],
+    ).toEqual(FLASHMINT_LEVERAGED_ZEROEX_BALANCER_FL_ABI)
+  })
+
   test('returns correct ABI for FlashMintLeveragedZeroEx on Base', async () => {
     expect(FlashMintAbis[Contracts[8453].FlashMintLeveragedZeroEx]).toEqual(
       FLASHMINT_LEVERAGED_ZEROEX_ABI,
--- src/utils/abis.ts
+++ src/utils/abis.ts
@@ -1,11 +1,14 @@
-import { base, mainnet } from 'viem/chains'
+import { arbitrum, base, mainnet } from 'viem/chains'
 
 import FLASHMINT_LEVERAGED_ZEROEX_ABI from 'constants/abis/FlashMintLeveragedZeroEx.json'
+import FLASHMINT_LEVERAGED_ZEROEX_BALANCER_FL_ABI from 'constants/abis/FlashMintLeveragedZeroExBalancerFL.json'
 import { Contracts } from 'constants/contracts'
 
 import type { Address } from 'viem'
 
 export const FlashMintAbis: { [key: Address]: any } = {
+  [Contracts[arbitrum.id].FlashMintLeveragedZeroEx]:
+    FLASHMINT_LEVERAGED_ZEROEX_BALANCER_FL_ABI,
   [Contracts[base.id].FlashMintLeveragedZeroEx]: FLASHMINT_LEVERAGED_ZEROEX_ABI,
   [Contracts[mainnet.id].FlashMintLeveragedZeroEx]:
     FLASHMINT_LEVERAGED_ZEROEX_ABI,
