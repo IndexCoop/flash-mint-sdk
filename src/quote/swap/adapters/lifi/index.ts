7502b412a7cadce4a2bfb9f5ab79cd66eff88209
JD <jann.driessen@gmx.de>
2025-04-03 11:16:00 +0200

    remove logs

--- src/quote/swap/adapters/lifi/index.ts
+++ src/quote/swap/adapters/lifi/index.ts
@@ -61,8 +61,7 @@ export class LiFiSwapQuoteProvider implements SwapQuoteProviderV2 {
         },
       }
     } catch (error) {
-      console.warn('Error getting LiFi swap quote:')
-      console.log(error)
+      console.log('Error getting LiFi swap quote.')
       return null
     }
   }
