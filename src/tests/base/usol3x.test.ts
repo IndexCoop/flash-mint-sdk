94c623208cc23c3f5780d2f4c3c06ad23761e531
JD <jann.driessen@gmx.de>
2025-04-24 10:37:08 +0200

    lint

--- src/tests/base/usol3x.test.ts
+++ src/tests/base/usol3x.test.ts
@@ -1,133 +1,78 @@
-import { BigNumber } from "@ethersproject/bignumber";
-import { getTokenByChainAndSymbol } from "@indexcoop/tokenlists";
-import { ChainId } from "constants/chains";
-import { JsonRpcProvider } from "@ethersproject/providers";
-import { ETH } from "constants/tokens";
+import { BigNumber } from '@ethersproject/bignumber'
+import { getTokenByChainAndSymbol } from '@indexcoop/tokenlists'
+import { ChainId } from 'constants/chains'
+import { ETH } from 'constants/tokens'
 import {
-    type TestFactory,
-    getTestFactoryZeroExV2,
-    transferFromWhale,
-    wei,
-    wrapETH,
-} from "tests/utils";
+  type TestFactory,
+  getTestFactoryZeroExV2,
+  wei,
+  wrapETH,
+} from 'tests/utils'
 
-describe("uSOL3x (Base)", () => {
-    const chainId = ChainId.Base;
-    const indexToken = getTokenByChainAndSymbol(chainId, "uSOL3x");
-    const usdc = getTokenByChainAndSymbol(chainId, "USDC");
-    const weth = getTokenByChainAndSymbol(chainId, "WETH");
-    let factory: TestFactory;
-    beforeAll(async () => {
-        factory = getTestFactoryZeroExV2(5, chainId);
-    });
+describe('uSOL3x (Base)', () => {
+  const chainId = ChainId.Base
+  const indexToken = getTokenByChainAndSymbol(chainId, 'uSOL3x')
+  const usdc = getTokenByChainAndSymbol(chainId, 'USDC')
+  const weth = getTokenByChainAndSymbol(chainId, 'WETH')
+  let factory: TestFactory
+  beforeAll(async () => {
+    factory = getTestFactoryZeroExV2(5, chainId)
+  })
 
-    test.skip("can mint with ETH", async () => {
-        await factory.fetchQuote({
-            chainId,
-            isMinting: true,
-            inputToken: ETH,
-            outputToken: indexToken,
-            indexTokenAmount: wei("1").toString(),
-            inputTokenAmount: wei("0.5").toString(),
-            slippage: 0.5,
-        });
-        await factory.executeTx();
-    });
+  test.skip('can mint with ETH', async () => {
+    await factory.fetchQuote({
+      chainId,
+      isMinting: true,
+      inputToken: ETH,
+      outputToken: indexToken,
+      indexTokenAmount: wei('1').toString(),
+      inputTokenAmount: wei('0.5').toString(),
+      slippage: 0.5,
+    })
+    await factory.executeTx()
+  })
 
-    [
-        ["1", "500"],
-        ["10", "5000"],
-        ["100", "50000"],
-        //["1000", "500000"],
-    ].forEach(([setAmount, usdcAmount]) => {
-        describe(`SetAmount: ${setAmount} - usdcAmount ${usdcAmount}`, () => {
-            let quote: any;
-            const whale = "0x621e7c767004266c8109e83143ab0Da521B650d6";
-            beforeAll(async () => {
-                const alchemyUrl = process.env.BASE_ALCHEMY_API;
-                const localHostProvider = new JsonRpcProvider(
-                    "http://localhost:8453"
-                );
-                // Note: Resetting fork to latest block to make sure quote is in line with state of the local fork
-                await localHostProvider.send("hardhat_reset", [
-                    {
-                        forking: {
-                            jsonRpcUrl: alchemyUrl,
-                        },
-                    },
-                ]);
-                quote = await factory.fetchQuote({
-                    chainId,
-                    isMinting: true,
-                    inputToken: usdc,
-                    outputToken: indexToken,
-                    indexTokenAmount: wei(setAmount).toString(),
-                    inputTokenAmount: wei(usdcAmount, 6).toString(),
-                    slippage: 0.5,
-                });
-            });
+  test.skip('can mint with WETH', async () => {
+    const quote = await factory.fetchQuote({
+      chainId,
+      isMinting: true,
+      inputToken: weth,
+      outputToken: indexToken,
+      indexTokenAmount: wei('1').toString(),
+      inputTokenAmount: wei('0.5').toString(),
+      slippage: 0.5,
+    })
+    await wrapETH(
+      quote.inputAmount.mul(BigNumber.from(2)),
+      factory.getSigner(),
+      chainId,
+    )
+    await factory.executeTx()
+  })
 
-            test.only("can obtain quote", () => {
-                expect(quote != null).toBe(true);
-            });
+  test.skip('can redeem to ETH', async () => {
+    await factory.fetchQuote({
+      chainId,
+      isMinting: false,
+      inputToken: indexToken,
+      outputToken: ETH,
+      indexTokenAmount: wei('1').toString(),
+      inputTokenAmount: wei('1').toString(),
+      slippage: 0.5,
+    })
+    await factory.executeTx()
+  })
 
-            test.skip("can mint", async () => {
-                if (quote == null) {
-                    throw new Error("Can't mint without quote");
-                }
-                await transferFromWhale(
-                    whale,
-                    factory.getSigner().address,
-                    quote.inputOutputAmount,
-                    quote.inputToken.address,
-                    factory.getProvider()
-                );
-                await factory.executeTx();
-            });
-        });
-    });
-
-    test.skip("can mint with WETH", async () => {
-        const quote = await factory.fetchQuote({
-            chainId,
-            isMinting: true,
-            inputToken: weth,
-            outputToken: indexToken,
-            indexTokenAmount: wei("1").toString(),
-            inputTokenAmount: wei("0.5").toString(),
-            slippage: 0.5,
-        });
-        await wrapETH(
-            quote.inputAmount.mul(BigNumber.from(2)),
-            factory.getSigner(),
-            chainId
-        );
-        await factory.executeTx();
-    });
-
-    test.skip("can redeem to ETH", async () => {
-        await factory.fetchQuote({
-            chainId,
-            isMinting: false,
-            inputToken: indexToken,
-            outputToken: ETH,
-            indexTokenAmount: wei("1").toString(),
-            inputTokenAmount: wei("1").toString(),
-            slippage: 0.5,
-        });
-        await factory.executeTx();
-    });
-
-    test.skip("can redeem to USDC", async () => {
-        await factory.fetchQuote({
-            chainId,
-            isMinting: false,
-            inputToken: indexToken,
-            outputToken: usdc,
-            indexTokenAmount: wei("1").toString(),
-            inputTokenAmount: wei("1").toString(),
-            slippage: 0.5,
-        });
-        await factory.executeTx();
-    });
-});
+  test.skip('can redeem to USDC', async () => {
+    await factory.fetchQuote({
+      chainId,
+      isMinting: false,
+      inputToken: indexToken,
+      outputToken: usdc,
+      indexTokenAmount: wei('1').toString(),
+      inputTokenAmount: wei('1').toString(),
+      slippage: 0.5,
+    })
+    await factory.executeTx()
+  })
+})
