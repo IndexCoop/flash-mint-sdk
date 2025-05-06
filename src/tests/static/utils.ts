b71f93f0dc4c610da32557dcd9e05c18cc697b6a
JD <jann.driessen@gmx.de>
2025-05-06 11:38:19 +0200

    test: update mainnet tests to use wbtc

--- src/tests/static/utils.ts
+++ src/tests/static/utils.ts
@@ -1,6 +1,7 @@
 import type { TestCase } from 'tests/utils'
+import { base } from 'viem/chains'
 
-export function buildTestCases(symbols: string[]) {
+export function buildTestCases(symbols: string[], chainId: number): TestCase[] {
   const testCasesEth: TestCase[] = symbols.map((symbol) => ({
     indexToken: symbol,
     setAmount: '1',
@@ -13,5 +14,28 @@ export function buildTestCases(symbols: string[]) {
     inputAmount: '5000',
     inputToken: 'USDC',
   }))
-  return [...testCasesEth, ...testCasesUsdc]
+  const testCasesBtc: TestCase[] = symbols.map((symbol) => ({
+    indexToken: symbol,
+    setAmount: '1',
+    inputAmount: '1',
+    inputToken: chainId === base.id ? 'cbBTC' : 'WBTC',
+  }))
+  return [...testCasesEth, ...testCasesUsdc, ...testCasesBtc]
+}
+
+export function getWhale(symbol: string, chainId: number): string {
+  switch (chainId) {
+    case base.id:
+      return symbol === 'USDC'
+        ? '0x621e7c767004266c8109e83143ab0Da521B650d6'
+        : '0xA8401BE86a024763A6092Be372b8762e279769F1' // cbBTC
+    case 1:
+      return symbol === 'USDC'
+        ? '0x768145BcC76a744E7F267b515d6E2488BdA48f0d'
+        : '0xE940ae8cF59fE2709BBc572CBAD2633fB45Abf46' // WBTC
+    case 42161:
+      return '0x0F896345B538Ac140Ac84f3367a65a34eFD8fcBf' // WBTC
+    default:
+      throw new Error('Unsupported chainId')
+  }
 }
