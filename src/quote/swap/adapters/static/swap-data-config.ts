428ee7b2f039192ca70339762f29b1c8518f3bd6
JD <jann.driessen@gmx.de>
2025-05-07 14:36:35 +0200

    feat: add static swap data for iceth-steth

--- src/quote/swap/adapters/static/swap-data-config.ts
+++ src/quote/swap/adapters/static/swap-data-config.ts
@@ -138,6 +138,24 @@ export const SwapDataConfig: Readonly<{
       },
     },
     'icETH': {
+      '0xae7ab96520DE3A18E5e111B5EaAb095312D7fE84': {
+        contract: 'ExchangeIssuanceLeveraged',
+        swapDataDebtForCollateral: {
+          path: [
+            '0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE',
+            '0xae7ab96520DE3A18E5e111B5EaAb095312D7fE84',
+          ],
+          fees: [],
+          exchange: 4,
+          pool: '0xDC24316b9AE028F1497c275EB9192a3Ea0f67022',
+        },
+        swapDataInputToken: {
+          path: ['0xae7ab96520DE3A18E5e111B5EaAb095312D7fE84'],
+          fees: [],
+          exchange: 4,
+          pool: '0xDC24316b9AE028F1497c275EB9192a3Ea0f67022',
+        },
+      },
       '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2': {
         contract: 'ExchangeIssuanceLeveraged',
         swapDataDebtForCollateral: {
