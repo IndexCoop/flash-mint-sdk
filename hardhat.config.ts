6e1860beed5f3ce066f5643c6f4c20acc0c726fe
christn <c.k.e.koopmann@gmail.com>
2025-05-15 12:11:38 +0800

    refactor: Remove unnecessary base_rpc url from hardhat config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -7,22 +7,9 @@ import "tsconfig-paths/register";
 
 dotenv.config();
 
-const ALCHEMY_KEY = process.env.ALCHEMY_API_KEY;
-if (!ALCHEMY_KEY) throw new Error("Missing ALCHEMY_API_KEY");
-
-const BASE_RPC = `https://base-mainnet.g.alchemy.com/v2/${ALCHEMY_KEY}`;
-
 const config: HardhatUserConfig = {
     solidity: "0.8.17",
     mocha: { timeout: 100000 },
-    networks: {
-        hardhat: {
-            forking: {
-                url: BASE_RPC,
-                // optional: blockNumber: 12345678
-            },
-        },
-    },
 };
 
 export default config;
